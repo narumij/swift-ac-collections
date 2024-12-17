3255b36f83f729a861f21c6cabc52d8221766932
narumij <narumij@gmail.com>
2024-12-18 04:01:17 +0900

    renames

---
+++ Sources/RedBlackTreeModule/UnsafeHandle/___UnsafeHandleCommon.swift
@@ -0,0 +1,46 @@
+// Copyright 2024 narumij
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This code is based on work originally distributed under the Apache License 2.0 with LLVM Exceptions:
+//
+// Copyright © 2003-2024 The LLVM Project.
+// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+// The original license can be found at https://llvm.org/LICENSE.txt
+//
+// This Swift implementation includes modifications and adaptations made by narumij.
+
+import Foundation
+
+@usableFromInline
+protocol ___UnsafeHandleCommon {
+  associatedtype VC: ValueComparer
+}
+
+extension ___UnsafeHandleCommon {
+
+  @usableFromInline
+  typealias _Key = VC._Key
+
+  @usableFromInline
+  typealias Element = VC.Element
+
+  @inlinable func __value_(_ e: VC.Element) -> _Key {
+    VC.__key(e)
+  }
+
+  @inlinable func value_comp(_ a: _Key, _ b: _Key) -> Bool {
+    VC.value_comp(a, b)
+  }
+}
+
--- Sources/RedBlackTreeModule/__tree/ValueComparer.swift
+++ Sources/RedBlackTreeModule/__tree/ValueComparer.swift
@@ -29,26 +29,3 @@ protocol ValueComparer {
   static func __key(_: Element) -> _Key
   static func value_comp(_: _Key, _: _Key) -> Bool
 }
-
-@usableFromInline
-protocol ___UnsafeHandleCommon {
-  associatedtype VC: ValueComparer
-}
-
-extension ___UnsafeHandleCommon {
-
-  @usableFromInline
-  typealias _Key = VC._Key
-
-  @usableFromInline
-  typealias Element = VC.Element
-
-  @inlinable func __value_(_ e: VC.Element) -> _Key {
-    VC.__key(e)
-  }
-
-  @inlinable func value_comp(_ a: _Key, _ b: _Key) -> Bool {
-    VC.value_comp(a, b)
-  }
-}
-

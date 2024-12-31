e048709efdfadc6186a6785f90152485b299e73c
narumij <narumij@gmail.com>
2024-12-31 17:24:06 +0900

    tests

---
+++ Tests/treeTests/AllocationTests.swift
@@ -0,0 +1,125 @@
+//
+//  AllocationTests.swift
+//  swift-ac-collections
+//
+//  Created by narumij on 2024/12/31.
+//
+
+import XCTest
+
+@testable import RedBlackTreeModule
+
+final class AllocationTests: XCTestCase {
+
+  typealias Tree = RedBlackTreeSet<Int>.Tree
+  typealias Storage = RedBlackTreeSet<Int>.Tree.Storage
+  typealias Header = RedBlackTreeSet<Int>.Tree.Header
+  typealias Node = RedBlackTreeSet<Int>.Tree.Node
+
+  func test0() throws {
+    do {
+      let storage: Storage = .create(withCapacity: 0)
+      XCTAssertEqual(storage.capacity, 0)
+      XCTAssertEqual(storage.tree.header.capacity, 0)
+      XCTAssertEqual(storage.tree.capacity, 0)
+      // Bufferの内部コードでのサイズと、外部コードでのサイズが異なるので、注意が必要みたい
+      //        XCTAssertEqual(storage.tree.capacity, MemoryLayout<Header>.size * 0)
+    }
+    do {
+      let storage: Storage = .create(withCapacity: 1)
+      XCTAssertEqual(storage.capacity, 1)
+      XCTAssertEqual(storage.tree.header.capacity, 1)
+      XCTAssertEqual(storage.tree.capacity, 1)
+      // Bufferの内部コードでのサイズと、外部コードでのサイズが異なるので、注意が必要みたい
+      //        XCTAssertEqual(storage.tree.capacity, MemoryLayout<Header>.size * 1)
+    }
+    do {
+      let storage: Storage = .create(withCapacity: 2)
+      XCTAssertEqual(storage.capacity, 2)
+      XCTAssertEqual(storage.tree.header.capacity, 2)
+      XCTAssertEqual(storage.tree.capacity, 2)
+      // Bufferの内部コードでのサイズと、外部コードでのサイズが異なるので、注意が必要みたい
+      //        XCTAssertEqual(storage.tree.capacity, MemoryLayout<Header>.size * 2)
+    }
+  }
+
+  func test1() throws {
+    do {
+      // countがゼロになった場合、一旦リセットしてもいい気はするが、しばらく保留
+      
+      let storage: Storage = .create(withCapacity: 5)
+      for i in 0..<5 {
+        XCTAssertEqual(storage.tree.__construct_node(-1), i)
+      }
+      XCTAssertEqual(storage.capacity, 5)
+      XCTAssertEqual(storage.tree.header.initializedCount, 5)
+      XCTAssertEqual(storage.tree.count, 5)
+      XCTAssertEqual(storage.tree.header.destroyCount, 0)
+      for i in (0..<5).reversed() {
+        storage.tree.destroy(i)
+      }
+      XCTAssertEqual(storage.capacity, 5)
+      XCTAssertEqual(storage.tree.header.initializedCount, 5)
+      XCTAssertEqual(storage.tree.count, 0)
+      XCTAssertEqual(storage.tree.header.destroyCount, 5)
+
+      do {
+        let initializedCount = storage.tree.header.initializedCount
+        // ストレージのリファレンスが2になる
+        var set = RedBlackTreeSet(_storage: storage)
+        XCTAssertEqual(set._storage.capacity, 5)
+        XCTAssertEqual(set._storage.tree.header.initializedCount, 5)
+        XCTAssertEqual(set._storage.tree.count, 0)
+        XCTAssertEqual(set._storage.tree.header.destroyCount, 5)
+        set._ensureUniqueAndCapacity(minimumCapacity: 1)
+        // リファレンスが2なので、CoWが発火する
+        XCTAssertFalse(storage === set._storage)
+        // ノードの配置はバラバラになりうるので、初期化されたサイズを下回ると、壊れる
+        XCTAssertGreaterThanOrEqual(set._storage.capacity, initializedCount)
+        XCTAssertGreaterThanOrEqual(set._storage.capacity, 1)
+        XCTAssertEqual(set._storage.count, 0)
+      }
+
+      do {
+        let initializedCount = storage.tree.header.initializedCount
+        // ストレージのリファレンスが2になる
+        var set = RedBlackTreeSet(_storage: storage)
+        XCTAssertEqual(set._storage.capacity, 5)
+        XCTAssertEqual(set._storage.tree.header.initializedCount, 5)
+        XCTAssertEqual(set._storage.tree.count, 0)
+        XCTAssertEqual(set._storage.tree.header.destroyCount, 5)
+        set._ensureUniqueAndCapacity(minimumCapacity: 15)
+        // リファレンスが2なので、CoWが発火する
+        XCTAssertFalse(storage === set._storage)
+        // ノードの配置はバラバラになりうるので、初期化されたサイズを下回ると、壊れる
+        XCTAssertGreaterThanOrEqual(set._storage.capacity, initializedCount)
+        XCTAssertGreaterThanOrEqual(set._storage.capacity, 15)
+        XCTAssertEqual(set._storage.count, 0)
+      }
+    }
+  }
+
+  func test2() throws {
+    do {
+      var A = RedBlackTreeSet<Int>()
+      let B = A
+      XCTAssertTrue(A._storage === B._storage)
+      A._ensureUnique()
+      XCTAssertTrue(A._storage !== B._storage)
+    }
+
+    do {
+      var A = RedBlackTreeSet<Int>()
+      XCTAssertEqual(A._copyCount, 0)
+      // treeの保持が単一ではない場合
+      let C = A._storage.tree
+      defer { _fixLifetime(C) }
+      A._ensureUnique()
+      // 弱ユニーク化は発火しないが
+      XCTAssertEqual(A._copyCount, 0)
+      A._strongEnsureUnique()
+      // 強ユニーク化は発火すること
+      XCTAssertEqual(A._copyCount, 1)
+    }
+  }
+}
---
+++ Tests/treeTests/MultisetCopyOnWriteTests.swift
@@ -0,0 +1,134 @@
+//
+//  RedBlackTreeCopyOnWriteTests.swift
+//  swift-ac-collections
+//
+//  Created by narumij on 2024/12/31.
+//
+
+import RedBlackTreeModule
+import XCTest
+
+final class MultisetCopyOnWriteTests: XCTestCase {
+
+  let count = 2_000_000
+
+  func testSet1() throws {
+    var set = RedBlackTreeMultiset<Int>()
+    XCTAssertEqual(set._copyCount, 0)
+    set.insert(0)
+    XCTAssertGreaterThanOrEqual(set._copyCount, 1) // 挿入に備え、かつ消費
+    set.insert(0)
+    XCTAssertGreaterThanOrEqual(set._copyCount, 2) // 挿入に備え、かつ消費
+    set.insert(0)
+    XCTAssertGreaterThanOrEqual(set._copyCount, 3) // 挿入に備え、かつ消費
+  }
+
+  func testSet2() throws {
+    var set = RedBlackTreeMultiset<Int>(minimumCapacity: 1)
+    XCTAssertEqual(set._copyCount, 0)
+    set.insert(0)
+    XCTAssertEqual(set._copyCount, 0)
+    set.remove(0)
+    XCTAssertEqual(set._copyCount, 0)
+    _ = set.lowerBound(0)
+    _ = set.upperBound(0)
+    for s in set {
+      print(s)
+    }
+    set.forEach {
+      print($0)
+    }
+    print(set.map{ $0 })
+    print(set.filter{ $0 != 0 })
+    print(set.reduce(0, +))
+    print(set.reduce(into: []) { $0.append($1) })
+    XCTAssertEqual(set._copyCount, 0)
+  }
+  
+  func testSet3() throws {
+    var tree = RedBlackTreeMultiset<Int>(0 ..< 20)
+    tree._copyCount = 0
+    for v in tree {
+      tree.remove(v) // strong ensure unique
+    }
+    XCTAssertEqual(tree.count, 0)
+    XCTAssertEqual(tree._copyCount, 1) // multi setの場合、インデックスを破壊するので1とする
+  }
+  
+  func testSet3_2() throws {
+    var tree = RedBlackTreeMultiset<Int>(0 ..< 20)
+    tree._copyCount = 0
+    for v in tree.map({ $0 }) {
+      tree.remove(v) // strong ensure unique
+    }
+    XCTAssertEqual(tree.count, 0)
+    XCTAssertEqual(tree._copyCount, 0) // mapで操作が済んでいるので、インデックス破壊の心配がない
+  }
+
+  func testSet4() throws {
+    var tree = RedBlackTreeMultiset<Int>(0 ..< 20)
+    tree._copyCount = 0
+    tree.forEach { v in
+      tree.remove(v)
+    }
+    XCTAssertEqual(tree.count, 0)
+    XCTAssertEqual(tree._copyCount, 1)
+  }
+
+  func testSet5() throws {
+    var tree = RedBlackTreeMultiset<Int>(0 ..< 20)
+    tree._copyCount = 0
+    for v in tree.map({ $0}) {
+      tree.remove(v)
+    }
+    XCTAssertEqual(tree.count, 0)
+    XCTAssertEqual(tree._copyCount, 0)
+  }
+  
+  func testSet6() throws {
+    var tree = RedBlackTreeMultiset<Int>(0 ..< 20)
+    tree._copyCount = 0
+    for v in tree.filter({ _ in true }) {
+      tree.remove(v)
+    }
+    XCTAssertEqual(tree.count, 0)
+    XCTAssertEqual(tree._copyCount, 0)
+  }
+
+  func testSet3000() throws {
+    let count = 1500
+    var loopCount = 0
+    var xy: [Int: RedBlackTreeMultiset<Int>] = [1: .init(0 ..< count)]
+    xy[1]?._copyCount = 0
+    let N = 100
+    for i in 0 ..< count / N {
+      loopCount += 1
+      if let lo = xy[1]?.lowerBound(i * N),
+         let hi = xy[1]?.upperBound(i * N + N) {
+        xy[1]?.removeSubrange(lo ..< hi)
+      }
+    }
+    XCTAssertTrue(xy[1]!._checkUnique())
+    XCTAssertEqual(xy[1]!.count, 0)
+    XCTAssertEqual(xy[1]!._copyCount, 0)
+    XCTAssertEqual(loopCount, count / N)
+  }
+
+  func testSet4000() throws {
+    let count = 1500
+    var xy: [Int: RedBlackTreeMultiset<Int>] = [1: .init(0 ..< count)]
+    xy[1]?._copyCount = 0
+    let N = 100
+    var loopCount = 0
+    for i in 0 ..< count / N {
+      loopCount += 1
+      xy[1]?[(i * N) ..< (i * N + N)].enumerated().forEach { i, v in
+        xy[1]?.remove(at: i)
+      }
+    }
+    XCTAssertTrue(xy[1]!._checkUnique())
+    XCTAssertEqual(xy[1]!.count, 0)
+    XCTAssertEqual(xy[1]!._copyCount, 0)
+    XCTAssertEqual(loopCount, count / N)
+  }
+}

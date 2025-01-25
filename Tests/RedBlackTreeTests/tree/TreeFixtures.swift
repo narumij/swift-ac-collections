046c467a6f429c283222f3db1c1834b2a8619187
narumij <narumij@gmail.com>
2025-01-25 18:41:36 +0900

    test

---
+++ Tests/RedBlackTreeTests/tree/TreeFixtures.swift
@@ -0,0 +1,173 @@
+import XCTest
+
+#if DEBUG
+  @testable import RedBlackTreeModule
+#else
+  import RedBlackTreeModule
+#endif
+
+#if DEBUG
+  extension ___RedBlackTree.___Node {
+    static var node: Self {
+      .init(__is_black_: false, __left_: .nullptr, __right_: .nullptr, __parent_: .nullptr)
+    }
+  }
+#endif
+
+#if DEBUG
+  class TreeFixtureBase<Element>:
+    XCTestCase,
+    ___tree_root_node, MemberProtocol, RootProtocol, EndNodeProtocol,
+    ___RedBlackTreeNodePoolProtocol
+  {
+    var __left_: _NodePtr = .nullptr
+    var __begin_node: _NodePtr = .end
+
+    var __nodes: [___RedBlackTree.___Node] = []
+    var __values: [Element] = []
+
+    var ___destroy_node: RedBlackTreeModule._NodePtr = .nullptr
+    var ___destroy_count: Int = 0
+
+    func __left_(_ p: _NodePtr) -> _NodePtr { p == .end ? __left_ : __nodes[p].__left_ }
+    func __left_(_ lhs: _NodePtr, _ rhs: _NodePtr) {
+      if lhs == .end {
+        __left_ = rhs
+      } else {
+        __nodes[lhs].__left_ = rhs
+      }
+    }
+    func __right_(_ p: _NodePtr) -> _NodePtr { __nodes[p].__right_ }
+    func __right_(_ lhs: _NodePtr, _ rhs: _NodePtr) { __nodes[lhs].__right_ = rhs }
+    func __parent_(_ p: _NodePtr) -> _NodePtr { __nodes[p].__parent_ }
+    func __parent_(_ lhs: _NodePtr, _ rhs: _NodePtr) { __nodes[lhs].__parent_ = rhs }
+    func __is_black_(_ p: _NodePtr) -> Bool { __nodes[p].__is_black_ }
+    func __is_black_(_ lhs: _NodePtr, _ rhs: Bool) { __nodes[lhs].__is_black_ = rhs }
+    func __parent_unsafe(_ p: _NodePtr) -> _NodePtr { __nodes[p].__parent_ }
+
+    func ___initialize(_ e: Element) -> _NodePtr {
+      let n = __nodes.count
+      __nodes.append(.node)
+      __values.append(e)
+      return n
+    }
+
+    func ___element(_ p: _NodePtr) -> Element {
+      __values[p]
+    }
+
+    func ___element(_ p: _NodePtr, _ e: Element) {
+      __values[p] = e
+    }
+
+    func __root(_ p: _NodePtr) {
+      __left_ = p
+    }
+
+    func clear() {
+      __left_ = .nullptr
+      __begin_node = .end
+      __nodes = []
+      ___clearDestroy()
+    }
+
+    override func setUpWithError() throws {
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+      clear()
+    }
+
+    override func tearDownWithError() throws {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+      clear()
+    }
+  }
+
+  class TreeFixture<Element: Comparable>:
+    TreeFixtureBase<Element>,
+    FindEqualProtocol, InsertNodeAtProtocol, InsertUniqueProtocol,
+    RemoveProtocol, EraseProtocol, EraseUniqueProtocol, CompareProtocol, CompareMultiProtocol,
+    BoundProtocol
+  {
+    func __key(_ e: Element) -> Element {
+      e
+    }
+
+    func __value_(_ p: _NodePtr) -> Element {
+      __values[p]
+    }
+
+    typealias Element = Element
+
+    var size: Int {
+      get { __nodes.count - ___destroy_count }
+      set {}
+    }
+
+    typealias _Key = Element
+
+    func value_comp(_ l: Element, _ r: Element) -> Bool {
+      l < r
+    }
+  }
+
+  class TreeFixture0_10_20: TreeFixture<Int> {
+
+    override func setUpWithError() throws {
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+      try super.setUpWithError()
+      __nodes = [
+        .init(__is_black_: true, __left_: 1, __right_: 2, __parent_: .end),
+        .init(__is_black_: false, __left_: nil, __right_: nil, __parent_: 0),
+        .init(__is_black_: false, __left_: nil, __right_: nil, __parent_: 0),
+      ]
+      __values = [
+        10,
+        0,
+        20,
+      ]
+      __root(0)
+      XCTAssertTrue(__tree_invariant(__root()))
+    }
+
+    override func tearDownWithError() throws {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+      try super.setUpWithError()
+    }
+  }
+
+  class TreeFixture0_1_2_3_4_5_6: TreeFixture<Int> {
+
+    override func setUpWithError() throws {
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+      try super.setUpWithError()
+
+      __nodes = [
+        .init(__is_black_: true, __left_: 1, __right_: 4, __parent_: .end),
+        .init(__is_black_: false, __left_: 2, __right_: 3, __parent_: 0),
+        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 1),
+        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 1),
+        .init(__is_black_: false, __left_: 5, __right_: 6, __parent_: 0),
+        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 4),
+        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 4),
+      ]
+      __values = [
+        3,
+        1,
+        0,
+        2,
+        4,
+        5,
+        6,
+      ]
+      __root(0)
+      //    size = tree.nodes.count
+      __begin_node = 2
+      XCTAssertTrue(__tree_invariant(__root()))
+    }
+
+    override func tearDownWithError() throws {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+      try super.setUpWithError()
+    }
+  }
+#endif
--- Tests/RedBlackTreeTests/tree/TreeTests.swift
+++ Tests/RedBlackTreeTests/tree/TreeTests.swift
@@ -1,10 +1,3 @@
-//
-//  TreeTests.swift
-//  swift-tree
-//
-//  Created by narumij on 2024/09/20.
-//
-
 import XCTest
 
 #if DEBUG
@@ -14,169 +7,6 @@ import XCTest
 #endif
 
 #if DEBUG
-  extension ___RedBlackTree.___Node {
-    static var node: Self {
-      .init(__is_black_: false, __left_: .nullptr, __right_: .nullptr, __parent_: .nullptr)
-    }
-  }
-#endif
-
-#if DEBUG
-  class TreeFixtureBase<Element>:
-    XCTestCase,
-    ___tree_root_node, MemberProtocol, RootProtocol, EndNodeProtocol,
-    ___RedBlackTreeNodePoolProtocol
-  {
-    var __left_: _NodePtr = .nullptr
-    var __begin_node: _NodePtr = .end
-
-    var __nodes: [___RedBlackTree.___Node] = []
-    var __values: [Element] = []
-
-    var ___destroy_node: RedBlackTreeModule._NodePtr = .nullptr
-    var ___destroy_count: Int = 0
-
-    func __left_(_ p: _NodePtr) -> _NodePtr { p == .end ? __left_ : __nodes[p].__left_ }
-    func __left_(_ lhs: _NodePtr, _ rhs: _NodePtr) {
-      if lhs == .end {
-        __left_ = rhs
-      } else {
-        __nodes[lhs].__left_ = rhs
-      }
-    }
-    func __right_(_ p: _NodePtr) -> _NodePtr { __nodes[p].__right_ }
-    func __right_(_ lhs: _NodePtr, _ rhs: _NodePtr) { __nodes[lhs].__right_ = rhs }
-    func __parent_(_ p: _NodePtr) -> _NodePtr { __nodes[p].__parent_ }
-    func __parent_(_ lhs: _NodePtr, _ rhs: _NodePtr) { __nodes[lhs].__parent_ = rhs }
-    func __is_black_(_ p: _NodePtr) -> Bool { __nodes[p].__is_black_ }
-    func __is_black_(_ lhs: _NodePtr, _ rhs: Bool) { __nodes[lhs].__is_black_ = rhs }
-    func __parent_unsafe(_ p: _NodePtr) -> _NodePtr { __nodes[p].__parent_ }
-
-    func ___initialize(_ e: Element) -> _NodePtr {
-      let n = __nodes.count
-      __nodes.append(.node)
-      __values.append(e)
-      return n
-    }
-
-    func ___element(_ p: _NodePtr) -> Element {
-      __values[p]
-    }
-
-    func ___element(_ p: _NodePtr, _ e: Element) {
-      __values[p] = e
-    }
-
-    func __root(_ p: _NodePtr) {
-      __left_ = p
-    }
-
-    func clear() {
-      __left_ = .nullptr
-      __begin_node = .end
-      __nodes = []
-      ___clearDestroy()
-    }
-
-    override func setUpWithError() throws {
-      // Put setup code here. This method is called before the invocation of each test method in the class.
-      clear()
-    }
-
-    override func tearDownWithError() throws {
-      // Put teardown code here. This method is called after the invocation of each test method in the class.
-      clear()
-    }
-  }
-
-  class TreeFixture<Element: Comparable>:
-    TreeFixtureBase<Element>,
-    FindEqualProtocol, InsertNodeAtProtocol, InsertUniqueProtocol,
-    RemoveProtocol, EraseProtocol, EraseUniqueProtocol
-  {
-    func __key(_ e: Element) -> Element {
-      e
-    }
-
-    func __value_(_ p: _NodePtr) -> Element {
-      __values[p]
-    }
-
-    typealias Element = Element
-
-    var size: Int {
-      get { __nodes.count - ___destroy_count }
-      set {}
-    }
-
-    typealias _Key = Element
-
-    func value_comp(_ l: Element, _ r: Element) -> Bool {
-      l < r
-    }
-  }
-
-  class TreeFixture0_10_20: TreeFixture<Int> {
-
-    override func setUpWithError() throws {
-      // Put setup code here. This method is called before the invocation of each test method in the class.
-      try super.setUpWithError()
-      __nodes = [
-        .init(__is_black_: true, __left_: 1, __right_: 2, __parent_: .end),
-        .init(__is_black_: false, __left_: nil, __right_: nil, __parent_: 0),
-        .init(__is_black_: false, __left_: nil, __right_: nil, __parent_: 0),
-      ]
-      __values = [
-        10,
-        0,
-        20,
-      ]
-      __root(0)
-      XCTAssertTrue(__tree_invariant(__root()))
-    }
-
-    override func tearDownWithError() throws {
-      // Put teardown code here. This method is called after the invocation of each test method in the class.
-      try super.setUpWithError()
-    }
-  }
-
-  class TreeFixture0_1_2_3_4_5_6: TreeFixture<Int> {
-
-    override func setUpWithError() throws {
-      // Put setup code here. This method is called before the invocation of each test method in the class.
-      try super.setUpWithError()
-
-      __nodes = [
-        .init(__is_black_: true, __left_: 1, __right_: 4, __parent_: .end),
-        .init(__is_black_: false, __left_: 2, __right_: 3, __parent_: 0),
-        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 1),
-        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 1),
-        .init(__is_black_: false, __left_: 5, __right_: 6, __parent_: 0),
-        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 4),
-        .init(__is_black_: true, __left_: nil, __right_: nil, __parent_: 4),
-      ]
-      __values = [
-        3,
-        1,
-        0,
-        2,
-        4,
-        5,
-        6,
-      ]
-      __root(0)
-      //    size = tree.nodes.count
-      __begin_node = 2
-      XCTAssertTrue(__tree_invariant(__root()))
-    }
-
-    override func tearDownWithError() throws {
-      // Put teardown code here. This method is called after the invocation of each test method in the class.
-      try super.setUpWithError()
-    }
-  }
-
   final class TreeBaseTests_EmptyNode: TreeFixtureBase<Void> {
 
     func testEmpty() throws {
@@ -380,6 +210,126 @@ import XCTest
         XCTAssertTrue(__insert_unique(i).__inserted)
         XCTAssertTrue(__tree_invariant(__root()))
       }
+    }
+  }
+
+  final class TreeTests_EmptyNode_Misc: TreeFixture<Int> {
+
+    override func setUpWithError() throws {
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+      clear()
+      _ = __insert_unique(0)
+      _ = __insert_unique(1)
+      _ = __insert_unique(2)
+      _ = __insert_unique(3)
+      _ = __insert_unique(4)
+      _ = __insert_unique(5)
+    }
+
+    override func tearDownWithError() throws {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+      clear()
+    }
+
+    func testMisc() throws {
+      XCTAssertTrue(___ptr_less_than(lower_bound(0), lower_bound(1)))
+      XCTAssertTrue(___ptr_less_than(lower_bound(1), lower_bound(2)))
+      XCTAssertTrue(___ptr_less_than(lower_bound(2), lower_bound(3)))
+      XCTAssertTrue(___ptr_less_than(lower_bound(3), lower_bound(4)))
+      XCTAssertTrue(___ptr_less_than(lower_bound(4), lower_bound(5)))
+      XCTAssertTrue(___ptr_less_than(lower_bound(5), .end))
+      XCTAssertFalse(___ptr_less_than(lower_bound(0), lower_bound(0)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(1), lower_bound(1)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(2), lower_bound(2)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(3), lower_bound(3)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(4), lower_bound(4)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(5), lower_bound(5)))
+      XCTAssertFalse(___ptr_less_than(.end, .end))
+      XCTAssertFalse(___ptr_less_than(lower_bound(1), lower_bound(0)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(2), lower_bound(1)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(3), lower_bound(2)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(4), lower_bound(3)))
+      XCTAssertFalse(___ptr_less_than(lower_bound(5), lower_bound(4)))
+      XCTAssertFalse(___ptr_less_than(.end, lower_bound(5)))
+    }
+
+    func testMisc2() throws {
+      XCTAssertFalse(___ptr_greator_than(lower_bound(0), lower_bound(1)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(1), lower_bound(2)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(2), lower_bound(3)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(3), lower_bound(4)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(4), lower_bound(5)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(5), .end))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(0), lower_bound(0)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(1), lower_bound(1)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(2), lower_bound(2)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(3), lower_bound(3)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(4), lower_bound(4)))
+      XCTAssertFalse(___ptr_greator_than(lower_bound(5), lower_bound(5)))
+      XCTAssertFalse(___ptr_greator_than(.end, .end))
+      XCTAssertTrue(___ptr_greator_than(lower_bound(1), lower_bound(0)))
+      XCTAssertTrue(___ptr_greator_than(lower_bound(2), lower_bound(1)))
+      XCTAssertTrue(___ptr_greator_than(lower_bound(3), lower_bound(2)))
+      XCTAssertTrue(___ptr_greator_than(lower_bound(4), lower_bound(3)))
+      XCTAssertTrue(___ptr_greator_than(lower_bound(5), lower_bound(4)))
+      XCTAssertTrue(___ptr_greator_than(.end, lower_bound(5)))
+    }
+
+    func testMisc3() throws {
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(0), lower_bound(1)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(1), lower_bound(2)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(2), lower_bound(3)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(3), lower_bound(4)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(4), lower_bound(5)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(5), .end))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(0), lower_bound(0)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(1), lower_bound(1)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(2), lower_bound(2)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(3), lower_bound(3)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(4), lower_bound(4)))
+      XCTAssertTrue(___ptr_less_than_or_equal(lower_bound(5), lower_bound(5)))
+      XCTAssertTrue(___ptr_less_than_or_equal(.end, .end))
+      XCTAssertFalse(___ptr_less_than_or_equal(lower_bound(1), lower_bound(0)))
+      XCTAssertFalse(___ptr_less_than_or_equal(lower_bound(2), lower_bound(1)))
+      XCTAssertFalse(___ptr_less_than_or_equal(lower_bound(3), lower_bound(2)))
+      XCTAssertFalse(___ptr_less_than_or_equal(lower_bound(4), lower_bound(3)))
+      XCTAssertFalse(___ptr_less_than_or_equal(lower_bound(5), lower_bound(4)))
+      XCTAssertFalse(___ptr_less_than_or_equal(.end, lower_bound(5)))
+    }
+
+    func testMisc4() throws {
+      XCTAssertFalse(___ptr_greator_than_or_equal(lower_bound(0), lower_bound(1)))
+      XCTAssertFalse(___ptr_greator_than_or_equal(lower_bound(1), lower_bound(2)))
+      XCTAssertFalse(___ptr_greator_than_or_equal(lower_bound(2), lower_bound(3)))
+      XCTAssertFalse(___ptr_greator_than_or_equal(lower_bound(3), lower_bound(4)))
+      XCTAssertFalse(___ptr_greator_than_or_equal(lower_bound(4), lower_bound(5)))
+      XCTAssertFalse(___ptr_greator_than_or_equal(lower_bound(5), .end))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(0), lower_bound(0)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(1), lower_bound(1)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(2), lower_bound(2)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(3), lower_bound(3)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(4), lower_bound(4)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(5), lower_bound(5)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(.end, .end))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(1), lower_bound(0)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(2), lower_bound(1)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(3), lower_bound(2)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(4), lower_bound(3)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(lower_bound(5), lower_bound(4)))
+      XCTAssertTrue(___ptr_greator_than_or_equal(.end, lower_bound(5)))
+    }
+
+    func testMisc5() throws {
+      XCTAssertFalse(___ptr_closed_range_contains(lower_bound(1), lower_bound(4), lower_bound(0)))
+      XCTAssertTrue(___ptr_closed_range_contains(lower_bound(1), lower_bound(4), lower_bound(1)))
+      XCTAssertTrue(___ptr_closed_range_contains(lower_bound(1), lower_bound(4), lower_bound(4)))
+      XCTAssertFalse(___ptr_closed_range_contains(lower_bound(1), lower_bound(4), lower_bound(5)))
+      XCTAssertFalse(___ptr_closed_range_contains(lower_bound(1), lower_bound(4), __end_node()))
+      XCTAssertFalse(___ptr_closed_range_contains(lower_bound(2), lower_bound(3), lower_bound(1)))
+      XCTAssertTrue(___ptr_closed_range_contains(lower_bound(2), lower_bound(3), lower_bound(2)))
+      XCTAssertTrue(___ptr_closed_range_contains(lower_bound(2), lower_bound(3), lower_bound(3)))
+      XCTAssertFalse(___ptr_closed_range_contains(lower_bound(2), lower_bound(3), lower_bound(4)))
+      XCTAssertFalse(___ptr_closed_range_contains(lower_bound(2), lower_bound(3), __end_node()))
     }
   }
 

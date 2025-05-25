2a902dd63f05049825fb1ae67fbae3673739076b
narumij <narumij@gmail.com>
2025-05-25 23:09:35 +0900

    tests

--- Tests/RedBlackTreeTests/RedBlackTreeSet/RedBlackTreeSet_1_SequenceTests.swift
+++ Tests/RedBlackTreeTests/RedBlackTreeSet/RedBlackTreeSet_1_SequenceTests.swift
@@ -7,6 +7,8 @@ import XCTest
 #endif
 
 final class RedBlackTreeSetSequenceTests: XCTestCase {
+  
+  // MARK: - 初期化内容とmap動作のチェック
 
   /// 空のセットの内容が空であること
   func test_emptyInitialization() {
@@ -15,7 +17,7 @@ final class RedBlackTreeSetSequenceTests: XCTestCase {
 
     let mappedElements = set.map { $0 }
 
-    // 事後条件: 空のものは空をかえすこと
+    // 事後条件:
     XCTAssertEqual(mappedElements, [], "空の配列をかえすこと")
   }
 
@@ -26,7 +28,7 @@ final class RedBlackTreeSetSequenceTests: XCTestCase {
 
     let mappedElements = set.map { $0 }
 
-    // 事後条件: 空のものは空をかえすこと
+    // 事後条件:
     XCTAssertEqual(mappedElements, [], "空をかえすこと")
   }
 
@@ -37,7 +39,7 @@ final class RedBlackTreeSetSequenceTests: XCTestCase {
 
     let mappedElements = set.map { $0 }
 
-    // 事後条件: 重複含めソート済み [1, 2, 3, 4, 5]
+    // 事後条件:
     let expected = [1, 2, 3, 4, 5]
     XCTAssertEqual(mappedElements, expected, "要素が重複無くソート済みであること")
   }
@@ -49,7 +51,7 @@ final class RedBlackTreeSetSequenceTests: XCTestCase {
 
     let mappedElements = set.map { $0 }
 
-    // 事後条件: ソート済み [1, 2, 3, 4]
+    // 事後条件:
     let expected = [1, 2, 3, 4]
     XCTAssertEqual(mappedElements, expected, "要素が重複無くソート済みであること")
   }
@@ -63,9 +65,9 @@ final class RedBlackTreeSetSequenceTests: XCTestCase {
     // 実行: mapで全要素取得
     let mappedElements = set.map { $0 }
 
-    // 事後条件: mappedElementsは昇順かつ重複なし
+    // 事後条件:
     let expected = Array(Set(elements)).sorted()
-    XCTAssertEqual(mappedElements, expected)
+    XCTAssertEqual(mappedElements, expected, "昇順かつ重複なしであること")
   }
 
   /// 内容が重複なく昇順であること(Arrayを初期化)
@@ -77,9 +79,9 @@ final class RedBlackTreeSetSequenceTests: XCTestCase {
     // 実行: makeIteratorを使用
     let iteratedElements = Array(set)
 
-    // 事後条件: 昇順かつ重複なし
+    // 事後条件:
     let expected = Array(Set(elements)).sorted()
-    XCTAssertEqual(iteratedElements, expected)
+    XCTAssertEqual(iteratedElements, expected, "昇順かつ重複なしであること")
   }
 
   /// 内容が重複なく昇順であること(sorted()との比較)
@@ -92,13 +94,15 @@ final class RedBlackTreeSetSequenceTests: XCTestCase {
     let mappedElements = set.map { $0 }
     let sortedElements = set.sorted()
 
-    // 事後条件: 順序も含めて一致
-    XCTAssertEqual(mappedElements, sortedElements)
+    // 事後条件:
+    XCTAssertEqual(mappedElements, sortedElements, "一致すること")
   }
 }
 
 extension RedBlackTreeSetSequenceTests {
   
+  // MARK: - その他Sequence動作のチェック
+
   /// forでRedBlackTreeSetの要素を正しく列挙できること
   func test_empty_set_for() {
     // 事前条件: 集合に[]を用意すること
@@ -111,8 +115,7 @@ extension RedBlackTreeSetSequenceTests {
     }
 
     // 事後条件:
-    // - 列挙結果が[]であること
-    XCTAssertEqual(elements, [])
+    XCTAssertEqual(elements, [], "空配列であること")
   }
   
   /// forでRedBlackTreeSetの要素を正しく列挙できること
@@ -127,8 +130,7 @@ extension RedBlackTreeSetSequenceTests {
     }
 
     // 事後条件:
-    // - 列挙結果が[1, 2, 3, 4, 5]であること
-    XCTAssertEqual(elements, [1, 2, 3, 4, 5])
+    XCTAssertEqual(elements, [1, 2, 3, 4, 5], "初期値通りであること")
   }
 
   /// forEachでRedBlackTreeSetの要素を正しく列挙できること
@@ -141,8 +143,7 @@ extension RedBlackTreeSetSequenceTests {
     set.forEach { elements.append($0) }
 
     // 事後条件:
-    // - 列挙結果が[1, 2, 3, 4, 5]であること
-    XCTAssertEqual(elements, [])
+    XCTAssertEqual(elements, [], "空配列であること")
   }
 
   /// forEachでRedBlackTreeSetの要素を正しく列挙できること
@@ -155,8 +156,7 @@ extension RedBlackTreeSetSequenceTests {
     set.forEach { elements.append($0) }
 
     // 事後条件:
-    // - 列挙結果が[1, 2, 3, 4, 5]であること
-    XCTAssertEqual(elements, [1, 2, 3, 4, 5])
+    XCTAssertEqual(elements, [1, 2, 3, 4, 5], "初期値通りであること")
   }
   
   /// filterでRedBlackTreeSetの要素を正しく絞り込めること
@@ -164,12 +164,11 @@ extension RedBlackTreeSetSequenceTests {
     // 事前条件: 集合に[]を用意すること
     let set = RedBlackTreeSet<Int>()
 
-    // 実行: forEachで要素を列挙すること
+    // 実行: filterで要素を偶数に絞り込む
     let elements: [Int] = set.filter{ $0 % 2 == 0 }
 
     // 事後条件:
-    // - 列挙結果が[1, 2, 3, 4, 5]であること
-    XCTAssertEqual(elements, [])
+    XCTAssertEqual(elements, [], "空配列であること")
   }
 
   /// filterでRedBlackTreeSetの要素を正しく絞り込めること
@@ -177,12 +176,11 @@ extension RedBlackTreeSetSequenceTests {
     // 事前条件: 集合に[1, 2, 3, 4, 5]を用意すること
     let set = RedBlackTreeSet([1, 2, 3, 4, 5])
 
-    // 実行: forEachで要素を列挙すること
+    // 実行: filterで要素を偶数に絞り込む
     let elements: [Int] = set.filter{ $0 % 2 == 0 }
 
     // 事後条件:
-    // - 列挙結果が[2, 4]であること
-    XCTAssertEqual(elements, [2, 4])
+    XCTAssertEqual(elements, [2, 4], "偶数のみであること")
   }
   
   /// reduceでRedBlackTreeSetの要素を正しくたたみこめること
@@ -194,8 +192,7 @@ extension RedBlackTreeSetSequenceTests {
     let element = set.reduce(0, +)
 
     // 事後条件:
-    // - 列挙結果が0であること
-    XCTAssertEqual(element, 0)
+    XCTAssertEqual(element, 0, "初期値であること")
   }
 
   /// reduceでRedBlackTreeSetの要素を正しくたたみこめること
@@ -207,7 +204,6 @@ extension RedBlackTreeSetSequenceTests {
     let element = set.reduce(0, +)
 
     // 事後条件:
-    // - 結果が15であること
-    XCTAssertEqual(element, 15)
+    XCTAssertEqual(element, 15, "合計値であること")
   }
 }

e7aedc7a7f902acaadd2140a90e4b3a29726cbfb
narumij <narumij@gmail.com>
2025-06-14 15:39:19 +0900

    some

--- Tests/Executable/main.swift
+++ Tests/Executable/main.swift
@@ -371,11 +371,17 @@ print(c.count)
 var a: RedBlackTreeSet<Int> = .init(100 ..< 10_000_000)
 a.removeSubrange(a.startIndex...)
 print(a.count)
-#elseif true
+#elseif false
 var a: RedBlackTreeSet<Int> = .init(0 ..< 10_000_000)
 var b: RedBlackTreeSet<Int> = .init(2 ..< 10_000_000)
 //let c = a.merging(b)
 a.formUnion(b)
+print(a.count)
+#elseif true
+var a: RedBlackTreeMultiSet<Int> = .init(0 ..< 10_000_000)
+var b: RedBlackTreeMultiSet<Int> = .init(2 ..< 10_000_000)
+//let c = a.merging(b)
+a.meld(b)
 print(a.count)
 #endif
 //#endif

0ee2b2f06ed4b0561f943f49422e0c44bb1d6c42
narumij <narumij@gmail.com>
2025-12-23 09:12:09 +0900

    keyvalue iterator

---
+++ Sources/RedBlackTreeModule/Iterator/RedBlackTreeReverseKeyValueIterator.swift
@@ -0,0 +1,143 @@
+//
+//  RedBlackTreeReverseKeyValueIterator.swift
+//  swift-ac-collections
+//
+//  Created by narumij on 2025/12/23.
+//
+
+import Foundation
+
+extension RedBlackTreeIterator.KeyValues {
+
+  @frozen
+  public struct Reversed: Sequence, IteratorProtocol {
+    public typealias Tree = ___Tree<Base>
+    public typealias _Value = Tree._Value
+
+    @usableFromInline
+    let __tree_: Tree
+
+    @usableFromInline
+    var _start, _end, _begin, _current, _next: _NodePtr
+
+    @inlinable
+    @inline(__always)
+    internal init(tree: Tree, start: _NodePtr, end: _NodePtr) {
+      self.__tree_ = tree
+      self._current = end
+      self._next = end == start ? end : __tree_.__tree_prev_iter(end)
+      self._start = start
+      self._end = end
+      self._begin = __tree_.__begin_node_
+    }
+
+    @inlinable
+    @inline(__always)
+    public mutating func next() -> (key: Base._Key, value: Base._MappedValue)? {
+      guard _current != _start else { return nil }
+      _current = _next
+      _next = _current != _begin ? __tree_.__tree_prev_iter(_current) : .nullptr
+      return (__tree_.__get_value(_current), __tree_.___mapped_value(_current))
+    }
+  }
+}
+
+extension RedBlackTreeIterator.KeyValues.Reversed {
+
+  @inlinable
+  @inline(__always)
+  public func forEach(_ body: (Tree.Index, Tree._Value) throws -> Void) rethrows {
+    try __tree_.___rev_for_each_(__p: _start, __l: _end) {
+      try body(__tree_.makeIndex(rawValue: $0), __tree_[$0])
+    }
+  }
+
+  @inlinable
+  @inline(__always)
+  public func ___forEach(_ body: (_NodePtr, Tree._Value) throws -> Void) rethrows {
+    try __tree_.___rev_for_each_(__p: _start, __l: _end) {
+      try body($0, __tree_[$0])
+    }
+  }
+}
+
+extension RedBlackTreeIterator.KeyValues.Reversed {
+
+  /// - Complexity: O(1)
+  @inlinable
+  @inline(__always)
+  public var indices: RedBlackTreeIndices<Base>.Reversed {
+    .init(tree: __tree_, start: _start, end: _end)
+  }
+}
+
+extension RedBlackTreeIterator.KeyValues.Reversed where Base: KeyValueComparer {
+
+  #if COMPATIBLE_ATCODER_2025
+    /// - Complexity: O(1)
+    @inlinable
+    @inline(__always)
+    public func keys() -> RedBlackTreeIterator<Base>.Keys.Reversed {
+      .init(tree: __tree_, start: _start, end: _end)
+    }
+
+    /// - Complexity: O(1)
+    @inlinable
+    @inline(__always)
+    public func values() -> RedBlackTreeIterator<Base>.MappedValues.Reversed {
+      .init(tree: __tree_, start: _start, end: _end)
+    }
+  #else
+    /// - Complexity: O(1)
+    @inlinable
+    @inline(__always)
+    public var keys: RedBlackTreeIterator<Base>.Keys.Reversed {
+      .init(tree: __tree_, start: _start, end: _end)
+    }
+
+    /// - Complexity: O(1)
+    @inlinable
+    @inline(__always)
+    public var values: RedBlackTreeIterator<Base>.MappedValues.Reversed {
+      .init(tree: __tree_, start: _start, end: _end)
+    }
+  #endif
+}
+
+extension RedBlackTreeIterator.KeyValues.Reversed {
+
+  @inlinable
+  @inline(__always)
+  public func ___node_positions() -> ___SafePointers<Base>.Reversed {
+    .init(tree: __tree_, start: _start, end: _end)
+  }
+}
+
+extension RedBlackTreeIterator.KeyValues.Reversed: Equatable
+where Base._Key: Equatable, Base._MappedValue: Equatable {
+
+  @inlinable
+  @inline(__always)
+  public static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs.isIdentical(to: rhs) || lhs.elementsEqual(rhs, by: ==)
+  }
+}
+
+extension RedBlackTreeIterator.KeyValues.Reversed: Comparable
+where Base._Key: Comparable, Base._MappedValue: Comparable {
+
+  @inlinable
+  @inline(__always)
+  public static func < (lhs: Self, rhs: Self) -> Bool {
+    !lhs.isIdentical(to: rhs) && lhs.lexicographicallyPrecedes(rhs, by: <)
+  }
+}
+
+#if swift(>=5.5)
+  extension RedBlackTreeIterator.KeyValues.Reversed: @unchecked Sendable
+  where Tree._Value: Sendable {}
+#endif
+
+// MARK: - Is Identical To
+
+extension RedBlackTreeIterator.KeyValues.Reversed: ___RedBlackTreeIsIdenticalTo {}

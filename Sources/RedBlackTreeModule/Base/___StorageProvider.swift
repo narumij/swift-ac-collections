f195a3aa5a5a41fa6f702a52eeaccc839fd86e51
narumij <narumij@gmail.com>
2025-12-24 20:58:12 +0900

    header, etc

--- Sources/RedBlackTreeModule/Base/___StorageProvider.swift
+++ Sources/RedBlackTreeModule/Base/___StorageProvider.swift
@@ -1,9 +1,24 @@
+// Copyright 2024-2025 narumij
 //
-//  ___StorageProvider.swift
-//  swift-ac-collections
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by narumij on 2025/12/24.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This code is based on work originally distributed under the Apache License 2.0 with LLVM Exceptions:
+//
+// Copyright © 2003-2024 The LLVM Project.
+// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+// The original license can be found at https://llvm.org/LICENSE.txt
+//
+// This Swift implementation includes modifications and adaptations made by narumij.
 
 @usableFromInline
 protocol ___StorageProvider: ___RedBlackTree___

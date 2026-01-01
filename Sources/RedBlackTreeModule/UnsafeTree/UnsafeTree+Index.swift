5d86d20f85aee0520eba28becda6b5efd39e7678
narumij <narumij@gmail.com>
2026-01-02 01:01:12 +0900

    header

--- Sources/RedBlackTreeModule/UnsafeTree/UnsafeTree+Index.swift
+++ Sources/RedBlackTreeModule/UnsafeTree/UnsafeTree+Index.swift
@@ -1,4 +1,4 @@
-// Copyright 2024-2025 narumij
+// Copyright 2024-2026 narumij
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,13 +14,11 @@
 //
 // This code is based on work originally distributed under the Apache License 2.0 with LLVM Exceptions:
 //
-// Copyright © 2003-2024 The LLVM Project.
+// Copyright © 2003-2025 The LLVM Project.
 // Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
 // The original license can be found at https://llvm.org/LICENSE.txt
 //
 // This Swift implementation includes modifications and adaptations made by narumij.
-
-
 
 extension UnsafeTree where Base: ___TreeIndex {
 

754492357f11d14e5d9a98606ecd7f4b27b11767
narumij <narumij@gmail.com>
2026-01-05 16:33:52 +0900

    fix tests

--- Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/UnsafeTreeV2.swift
+++ Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/UnsafeTreeV2.swift
@@ -285,31 +285,7 @@ extension UnsafeTreeV2 {
 
 // MARK: -
 
-extension UnsafeNode {
 
-  @inlinable
-  func debugDescription(resolve: (Pointer?) -> Int?) -> String {
-    let id = ___node_id_
-    let l = resolve(__left_)
-    let r = resolve(__right_)
-    let p = resolve(__parent_)
-    let color = __is_black_ ? "B" : "R"
-    #if DEBUG
-      let rc = ___recycle_count
-    #else
-      let rc = -1
-    #endif
-
-    return """
-      node[\(id)] \(color)
-        L: \(l.map(String.init) ?? "nil")
-        R: \(r.map(String.init) ?? "nil")
-        P: \(p.map(String.init) ?? "nil")
-        needsDeinit: \(___needs_deinitialize)
-        recycleCount: \(rc)
-      """
-  }
-}
 
 extension UnsafeTreeV2 {
 

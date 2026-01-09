a86238528df8ee690d726091f4ed742449e5f97d
narumij <narumij@gmail.com>
2026-01-10 03:52:39 +0900

    try

--- Tests/Executables/MultiRoundTrip/main.swift
+++ Tests/Executables/MultiRoundTrip/main.swift
@@ -7,9 +7,10 @@ let N = 2000
 //let N = 300_000
 #endif
 
+
+#if false
 var fixtures: [RedBlackTreeSet<Int>] = .init(repeating: .init(minimumCapacity: N), count: N)
 
-#if true
 for _ in 0..<1 {
   for j in 0..<N {
     for i in 0..<N {
@@ -22,4 +23,34 @@ for _ in 0..<1 {
 //    }
 //  }
 }
+#else
+enum Base<K,V>: ValueComparer {
+  static func __key(_: V) -> K {
+    fatalError()
+  }
+  static func value_comp(_: K, _: K) -> Bool {
+    fatalError()
+  }
+  typealias _Key = K
+  typealias _Value = V
+}
+
+struct Fixture<B,K> where B: ValueComparer, B._Key == K, K: Comparable {
+  
+  func comp(l: K, r: K) -> Bool {
+    l < r
+  }
+}
+
+var fixture = Fixture<Base<Int,Int>,Int>()
+
+var result = false
+for _ in 0..<10000000 {
+  let l = Int.random(in: 0..<Int.max)
+  let r = Int.random(in: 0..<Int.max)
+  result = fixture.comp(l: l, r: r)
+}
+print(result)
+
 #endif
+

2760749891ce4679b9348c26d433e986f8036512
narumij <narumij@gmail.com>
2026-01-11 23:56:20 +0900

    some clean

--- Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/___Handle/UnsafeTreeV2CommonHandle.swift
+++ Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/___Handle/UnsafeTreeV2CommonHandle.swift
@@ -1,24 +1,4 @@
-@frozen
-@usableFromInline
-struct UnsafeTreeV2CommonHandle<_Value> {
-  @inlinable
-  internal init(
-    header: UnsafeMutablePointer<UnsafeTreeV2Buffer<_Value>.Header>,
-    origin: UnsafeMutablePointer<UnsafeTreeV2Origin>
-  ) {
-    self.header = header
-    self.origin = origin
-  }
-  public typealias _Value = _Value
-  public typealias _NodePtr = UnsafeMutablePointer<UnsafeNode>
-  @usableFromInline let header: UnsafeMutablePointer<UnsafeTreeV2Buffer<_Value>.Header>
-  @usableFromInline let origin: UnsafeMutablePointer<UnsafeTreeV2Origin>
-}
-
 extension UnsafeTreeV2 {
-
-  @usableFromInline
-  typealias CommonHandle = UnsafeTreeV2CommonHandle<UnsafeTreeV2<Base>._Value>
 
   @inlinable
   @inline(__always)
@@ -95,17 +75,6 @@ extension UnsafeTreeV2 {
   ) rethrows -> R {
     try _buffer.withUnsafeMutablePointers { header, elements in
       return try body(&header.pointee, &elements.pointee)
-    }
-  }
-
-  @inlinable
-  @inline(__always)
-  internal func withandler<R>(
-    _ body: (UnsafeTreeV2CommonHandle<_Value>) throws -> R
-  ) rethrows -> R {
-    try _buffer.withUnsafeMutablePointers { header, elements in
-      let handle = UnsafeTreeV2CommonHandle<_Value>(header: header, origin: elements)
-      return try body(handle)
     }
   }
 }

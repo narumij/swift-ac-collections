61c61199f7888f24ea2d98243a29ccad994597e2
narumij <narumij@gmail.com>
2026-01-12 02:44:05 +0900

    header

--- Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/___Compare/SpecializeMode.swift
+++ Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/___Compare/SpecializeMode.swift
@@ -1,3 +1,24 @@
+// Copyright 2024-2026 narumij
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This code is based on work originally distributed under the Apache License 2.0 with LLVM Exceptions:
+//
+// Copyright © 2003-2025 The LLVM Project.
+// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+// The original license can be found at https://llvm.org/LICENSE.txt
+//
+// This Swift implementation includes modifications and adaptations made by narumij.
 
 /// Swift 6.2 2026/01/11時点で赤黒木の性能の肝of肝となる部分
 ///

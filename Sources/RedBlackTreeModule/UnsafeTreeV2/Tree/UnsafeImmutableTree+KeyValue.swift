c44b99ae1af2527f025afa0f6f3e079c03f24327
narumij <narumij@gmail.com>
2026-01-14 03:55:38 +0900

    preparation

---
+++ Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/UnsafeImmutableTree+KeyValue.swift
@@ -0,0 +1,25 @@
+//
+//  UnsafeImmutableTree+KeyValue.swift
+//  swift-ac-collections
+//
+//  Created by narumij on 2026/01/14.
+//
+
+extension UnsafeImmutableTree where Base: KeyValueComparer {
+
+  @inlinable
+  @inline(__always)
+  internal func ___mapped_value(_ __p: _NodePtr) -> Base._MappedValue {
+    Base.___mapped_value(UnsafePair<Base._Value>.valuePointer(__p)!.pointee)
+  }
+
+  @inlinable
+  @inline(__always)
+  internal func ___with_mapped_value<T>(
+    _ __p: _NodePtr, _ f: (inout Base._MappedValue) throws -> T
+  )
+    rethrows -> T
+  {
+    try Base.___with_mapped_value(&UnsafePair<Base._Value>.valuePointer(__p)!.pointee, f)
+  }
+}

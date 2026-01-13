246ec1bed0f269c13ad9af645bf1fcb6526d146b
narumij <narumij@gmail.com>
2026-01-13 12:19:44 +0900

    ensure strong unique

--- Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/UnsafeTreeV2+CopyOnWrite.swift
+++ Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/UnsafeTreeV2+CopyOnWrite.swift
@@ -61,7 +61,7 @@ extension UnsafeTreeV2 {
 }
 
 extension UnsafeTreeV2 {
-  
+
   @inlinable
   @inline(__always)
   internal mutating func _ensureUnique() {
@@ -70,12 +70,16 @@ extension UnsafeTreeV2 {
       self = self.copy()
     }
   }
-  
+
   @inlinable
   @inline(__always)
   internal mutating func _strongEnsureUnique() {
-    let isUnique = _buffer.isUniqueReference()
-    if !isUnique {
+    let isTreeUnique = _buffer.isUniqueReference()
+    let isPoolUnique =
+      _buffer.header._deallocator == nil
+      ? true : isKnownUniquelyReferenced(&_buffer.header._deallocator!)
+
+    if !isTreeUnique || !isPoolUnique {
       self = self.copy()
     }
   }
@@ -99,30 +103,30 @@ extension UnsafeTreeV2 {
   @inlinable
   @inline(__always)
   mutating func _ensureUniqueAndCapacity(to minimumCapacity: Int? = nil, linearly: Bool = false) {
-    
+
     let isUnique = _buffer.isUniqueReference()
 
     let growthCapacity: Int? = withMutableHeader { header in
-      
+
       let minimumCapacity = minimumCapacity ?? (header.count + 1)
-      
+
       let shouldExpand = header.freshPoolCapacity < minimumCapacity
-      
+
       guard shouldExpand || !isUnique else { return nil }
-      
+
       let growthCapacity = header.growthCapacity(to: minimumCapacity, linearly: linearly)
-      
+
       if !isUnique {
         return growthCapacity
       }
-      
+
       if shouldExpand {
         header.executeCapacityGrow(growthCapacity)
       }
-      
+
       return nil
     }
-    
+
     if let growthCapacity {
       self = self.copy(minimumCapacity: growthCapacity)
     }
@@ -164,10 +168,9 @@ extension UnsafeTreeV2Buffer.Header {
     //    if minimumCapacity <= 4 {
     //      return Swift.max(minimumCapacity, 4)
     //    }
-    
+
     // https://atcoder.jp/contests/abc411/submissions/72430351
 
     return Swift.max(minimumCapacity, freshPoolCapacity + max(freshPoolCapacity / 8, 2))
   }
 }
-

f30bf0002a1149a86b2e2fb2ebd8caa3a2321b99
narumij <narumij@gmail.com>
2026-01-21 05:55:54 +0900

    protocol refactoring

--- Sources/RedBlackTreeModule/__tree/interfaces/tree_interface+insert.swift
+++ Sources/RedBlackTreeModule/__tree/interfaces/tree_interface+insert.swift
@@ -19,8 +19,4 @@ protocol InsertNodeAtInterface: _NodePtrType {
 @usableFromInline
 protocol InsertUniqueInterface: InsertNodeAtInterface & _KeyType {
   func __find_equal(_ __v: _Key) -> (__parent: _NodePtr, __child: _NodeRef)
-  func
-    __insert_node_at(
-      _ __parent: _NodePtr, _ __child: _NodeRef,
-      _ __new_node: _NodePtr)
 }

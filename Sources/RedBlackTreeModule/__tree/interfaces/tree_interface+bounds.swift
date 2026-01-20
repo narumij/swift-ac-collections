48228c56c95cfbce248cd77cd52c375104a5c84b
narumij <narumij@gmail.com>
2026-01-21 07:05:22 +0900

    some

--- Sources/RedBlackTreeModule/__tree/interfaces/tree_interface+bounds.swift
+++ Sources/RedBlackTreeModule/__tree/interfaces/tree_interface+bounds.swift
@@ -5,12 +5,14 @@
 //  Created by narumij on 2026/01/18.
 //
 
+/// 2025年のシンプル化で代表格となったもの
 @usableFromInline
 protocol BoundInteface: _NodePtrType & _KeyType {
   func lower_bound(_ __v: _Key) -> _NodePtr
   func upper_bound(_ __v: _Key) -> _NodePtr
 }
 
+/// 2025年の改善で増えたもの
 @usableFromInline
 protocol BoundBothInterface: _NodePtrType & _KeyType {
   func __lower_bound_unique(_ __v: _Key) -> _NodePtr
@@ -19,6 +21,7 @@ protocol BoundBothInterface: _NodePtrType & _KeyType {
   func __upper_bound_multi(_ __v: _Key) -> _NodePtr
 }
 
+/// 昔からあるBoundインターフェースと同じシグネチャのもの
 @usableFromInline
 protocol BoundBasicInterface: _NodePtrType & _KeyType {
   func __lower_bound_multi(_ __v: _Key, _ __root: _NodePtr, _ __result: _NodePtr) -> _NodePtr

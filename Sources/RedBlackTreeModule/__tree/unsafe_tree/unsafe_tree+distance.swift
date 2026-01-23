849ad46259ea2ee94a8ff678cdc86473b93f1c01
narumij <narumij@gmail.com>
2026-01-23 13:55:46 +0900

    dual distance

--- Sources/RedBlackTreeModule/__tree/unsafe_tree/unsafe_tree+distance.swift
+++ Sources/RedBlackTreeModule/__tree/unsafe_tree/unsafe_tree+distance.swift
@@ -36,15 +36,38 @@ extension DistanceProtocol_ptr {
   @inlinable
   @inline(__always)
   internal func
+    ___dual_distance(_ __first: _InputIter, _ __last: _InputIter) -> difference_type
+  {
+    var __next = __first
+    var __prev = __first
+    var __r = 0
+    while __next != __last, __prev != __last {
+      if __next.___is_null, __prev.___is_null {
+        break
+      }
+      __next = __next.___is_null ? __next : __tree_next(__next)
+      __prev = __prev.___is_null ? __prev : __tree_prev_iter(__prev)
+      __r += 1
+    }
+    return __next == __last ? __r : -__r
+  }
+
+  @inlinable
+  @inline(__always)
+  internal func
     ___signed_distance(_ __first: _InputIter, _ __last: _InputIter) -> difference_type
   {
-    guard __first != __last else { return 0 }
-    var (__first, __last) = (__first, __last)
-    var sign = 1
-    if ___ptr_comp(__last, __first) {
-      swap(&__first, &__last)
-      sign = -1
-    }
-    return sign * __distance(__first, __last)
+    #if false
+      guard __first != __last else { return 0 }
+      var (__first, __last) = (__first, __last)
+      var sign = 1
+      if ___ptr_comp(__last, __first) {
+        swap(&__first, &__last)
+        sign = -1
+      }
+      return sign * __distance(__first, __last)
+    #else
+      return ___dual_distance(__first, __last)
+    #endif
   }
 }

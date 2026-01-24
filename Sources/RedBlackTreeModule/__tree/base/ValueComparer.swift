e8667065a89a0499e8b4df44b166f2bad87e41e6
narumij <narumij@gmail.com>
2026-01-25 04:46:10 +0900

    header

--- Sources/RedBlackTreeModule/__tree/base/ValueComparer.swift
+++ Sources/RedBlackTreeModule/__tree/base/ValueComparer.swift
@@ -1,9 +1,19 @@
+//===----------------------------------------------------------------------===//
 //
-//  ValueComparer.swift
-//  swift-ac-collections
+// This source file is part of the swift-ac-collections project
 //
-//  Created by narumij on 2026/01/24.
+// Copyright (c) 2024 - 2026 narumij.
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// This code is based on work originally distributed under the Apache License 2.0 with LLVM Exceptions:
+//
+// Copyright © 2003-2026 The LLVM Project.
+// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+// The original license can be found at https://llvm.org/LICENSE.txt
+//
+// This Swift implementation includes modifications and adaptations made by narumij.
+//
+//===----------------------------------------------------------------------===//
 
 /*
  日本語で整理

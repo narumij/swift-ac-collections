c4e6e620a59f23df3040c09a1cd429c939adebf5
narumij <narumij@gmail.com>
2026-01-27 09:16:52 +0900

    file

---
+++ Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/UnsafeTreeV2+IsIdenticalTo.swift
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-ac-collections project
+//
+// Copyright (c) 2024 - 2026 narumij.
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// This code is based on work originally distributed under the Apache License 2.0 with LLVM Exceptions:
+//
+// Copyright © 2003-2026 The LLVM Project.
+// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+// The original license can be found at https://llvm.org/LICENSE.txt
+//
+// This Swift implementation includes modifications and adaptations made by narumij.
+//
+//===----------------------------------------------------------------------===//
+
+extension UnsafeTreeV2 {
+
+  @inlinable
+  @inline(__always)
+  internal func isTriviallyIdentical(to other: UnsafeTreeV2) -> Bool {
+    self._buffer.buffer === other._buffer.buffer
+  }
+}

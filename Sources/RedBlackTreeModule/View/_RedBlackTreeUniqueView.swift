edb65482981bf8c646d7ea8213c4de937dbb6f33
narumij <narumij@gmail.com>
2026-01-31 07:07:27 +0900

    unique view

--- Sources/RedBlackTreeModule/View/_RedBlackTreeUniqueView.swift
+++ Sources/RedBlackTreeModule/View/_RedBlackTreeUniqueView.swift
@@ -5,10 +5,19 @@
 //  Created by narumij on 2026/01/31.
 //
 
-public struct _RedBlackTreeUniqueView<Base>: UnsafeTreeHost, _KeyBride, _PayloadValueBride, UnsafeIndexBinding, ___UnsafeBaseSequenceV2, ___UnsafeIndexV2
+/// C++を模倣したい場合に使う
+///
+public struct _RedBlackTreeUniqueView<Base>:
+  UnsafeMutableTreeHost,
+  _KeyBride,
+  _PayloadValueBride,
+  UnsafeIndexBinding,
+  ___UnsafeBaseSequenceV2,
+  ___UnsafeIndexV2
 where
   Base: ___TreeBase & ___TreeIndex
 {
+  
   @usableFromInline
   internal init(__tree_: UnsafeTreeV2<Base>) {
     self.__tree_ = __tree_
@@ -19,29 +28,29 @@ where
 }
 
 extension _RedBlackTreeUniqueView {
-  
-  /// - Complexity: O(1)
-  @inlinable
-  @inline(__always)
-  public var startIndex: Index { _startIndex }
 
   /// - Complexity: O(1)
   @inlinable
   @inline(__always)
-  public var endIndex: Index { _endIndex }
+  public __consuming func begin() -> Index { _startIndex }
+
+  /// - Complexity: O(1)
+  @inlinable
+  @inline(__always)
+  public __consuming func end() -> Index { _endIndex }
 }
 
 extension _RedBlackTreeUniqueView {
 
   /// - Complexity: O(log `count`)
   @inlinable
-  public func lowerBound(_ member: _Key) -> Index {
+  public __consuming func lowerBound(_ member: _Key) -> Index {
     ___index_lower_bound(member)
   }
 
   /// - Complexity: O(log `count`)
   @inlinable
-  public func upperBound(_ member: _Key) -> Index {
+  public __consuming func upperBound(_ member: _Key) -> Index {
     ___index_upper_bound(member)
   }
 }
@@ -50,7 +59,16 @@ extension _RedBlackTreeUniqueView {
 
   /// - Complexity: O(log `count`)
   @inlinable
-  public func firstIndex(of member: _Key) -> Index? {
+  public __consuming func findEqual(_ member: _Key) -> Index? {
     ___first_index(of: member)
+  }
+}
+
+extension _RedBlackTreeUniqueView {
+  
+  @inlinable
+  public mutating func erase(_ ptr: Index) -> Index {
+    __tree_.ensureUnique()
+    return ___erase(ptr)
   }
 }

115479b3690a5a7c428ea073ea9063fa09ab5242
narumij <narumij@gmail.com>
2026-02-12 05:59:23 +0900

    some

--- Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/Tag/_TrackingTagSealing.swift
+++ Sources/RedBlackTreeModule/UnsafeTreeV2/Tree/Tag/_TrackingTagSealing.swift
@@ -6,10 +6,6 @@
 //
 
 // 一度削除したが、情報欠落でデグレになったので、復活
-
-// タグをsalt付きに移行する場合、タグの生成は木だけが行うよう準備する必要がある
-// 競プロ用としてはsaltなしでいい。一般用として必要かどうかの判断となっていく
-
 public typealias _SealedTag = Result<_TrackingTagSealing, SealError>
 
 /// トラッキング番号解決の補助データ構造
@@ -24,8 +20,10 @@ extension _TrackingTagSealing {
   @inlinable
   static func seal(raw: _TrackingTag, seal: UnsafeNode.Seal) -> Self {
     switch raw {
-    case .end: return .end
-    case 0...: return .tag(raw: raw, seal: seal)
+    case .end:
+      return .end
+    case 0...:
+      return .tag(raw: raw, seal: seal)
     default:
       fatalError(.invalidIndex)
     }
@@ -33,7 +31,7 @@ extension _TrackingTagSealing {
 }
 
 extension _TrackingTagSealing: CustomStringConvertible {
-  
+
   public var description: String {
     switch self {
     case .end:
@@ -45,6 +43,6 @@ extension _TrackingTagSealing: CustomStringConvertible {
 }
 
 extension _TrackingTagSealing: CustomDebugStringConvertible {
-  
+
   public var debugDescription: String { description }
 }

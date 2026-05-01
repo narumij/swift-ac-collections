aa98433be5292cc8b1f093f066c719c3438ad739
narumij <narumij@gmail.com>
2026-05-01 20:57:48 +0900

    index

--- Sources/RedBlackTreeModule/Index/UnsafeIndexV4.swift
+++ Sources/RedBlackTreeModule/Index/UnsafeIndexV4.swift
@@ -7,10 +7,10 @@
 
 @frozen
 public struct UnsafeIndexV4: ~Escapable {
-  
+
   @usableFromInline
   package var ptr: _SealedPtr
-  
+
   @_lifetime(borrow _unsafe_end_node)
   @inlinable
   init(_unsafe_end_node: UnsafeMutablePointer<UnsafeNode>, ptr: _SealedPtr) {
@@ -18,8 +18,16 @@ public struct UnsafeIndexV4: ~Escapable {
   }
 }
 
+extension UnsafeIndexV4 {
+
+  public mutating func edit(_ transform: (_SealedPtr) -> _SealedPtr) {
+    ptr = transform(ptr)
+  }
+}
+
 extension RedBlackTreeSet {
 
+  //　コンパイルは通るが、CoWが効いた場合に、これだとやはりまずいかもしれない
   @_lifetime(borrow self)
   @inlinable
   func ___index(_ p: _SealedPtr) -> UnsafeIndexV4 {
@@ -27,6 +35,7 @@ extension RedBlackTreeSet {
     return unsafe _overrideLifetime(i, borrowing: self)
   }
 
+  //　コンパイルは通るが、CoWが効いた場合に、これだとやはりまずいかもしれない
   @_lifetime(borrow self)
   @inlinable
   func ___index_or_nil(_ p: _SealedPtr) -> UnsafeIndexV4? {
@@ -52,5 +61,86 @@ extension RedBlackTreeSet {
         to: end.ptr.purified)
     else { fatalError(.invalidIndex) }
     return d
+  }
+}
+
+extension RedBlackTreeSet {
+
+  /// - Complexity: O(1)
+  @inlinable
+  public func index(before i: UnsafeIndexV4) -> UnsafeIndexV4 {
+    var i = i
+    formIndex(before: &i)
+    return i
+  }
+
+  /// - Complexity: O(1)
+  @inlinable
+  public func index(after i: UnsafeIndexV4) -> UnsafeIndexV4 {
+    var i = i
+    formIndex(after: &i)
+    return i
+  }
+
+  /// - Complexity: O(`distance`)
+  @inlinable
+  public func index(_ i: UnsafeIndexV4, offsetBy distance: Int)
+    -> UnsafeIndexV4
+  {
+    var i = i
+    formIndex(&i, offsetBy: distance)
+    return i
+  }
+
+  /// - Complexity: O(`distance`)
+  @inlinable
+  public func index(
+    _ i: UnsafeIndexV4, offsetBy distance: Int, limitedBy limit: UnsafeIndexV4
+  )
+    -> UnsafeIndexV4?
+  {
+    var i = i
+    let result = formIndex(&i, offsetBy: distance, limitedBy: limit)
+    return result ? i : nil
+  }
+}
+
+extension RedBlackTreeSet {
+
+  /// - Complexity: O(1)
+  @inlinable
+  public func formIndex(before i: inout UnsafeIndexV4) {
+    i.edit { $0.purified.flatMap { ___tree_prev_iter($0.pointer).sealed } }
+  }
+
+  /// - Complexity: O(1)
+  @inlinable
+  public func formIndex(after i: inout UnsafeIndexV4) {
+    i.edit { $0.purified.flatMap { ___tree_next_iter($0.pointer).sealed } }
+  }
+
+  /// - Complexity: O(*d*)
+  @inlinable
+  public func formIndex(_ i: inout UnsafeIndexV4, offsetBy distance: Int) {
+    i.edit { $0.purified.flatMap { ___tree_adv_iter($0.pointer, distance).sealed } }
+  }
+
+  /// - Complexity: O(*d*)
+  @inlinable
+  public func formIndex(
+    _ i: inout UnsafeIndexV4,
+    offsetBy distance: Int,
+    limitedBy limit: UnsafeIndexV4
+  )
+    -> Bool
+  {
+    guard let ___i = i.ptr.purified.pointer
+    else { return false }
+
+    let __l = limit.ptr.purified.map(\.pointer)
+
+    return ___form_index(___i, offsetBy: distance, limitedBy: __l) {
+      i.ptr = $0.sealed
+    }
   }
 }

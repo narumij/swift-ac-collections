ea74d53e20ddb0f51a57a17a22ec7462042b2335
narumij <narumij@gmail.com>
2026-05-03 17:59:23 +0900

    fix test

--- Sources/RedBlackTreeModule/Iterator/UnsafeIterator/UnsafeIterator+Value.swift
+++ Sources/RedBlackTreeModule/Iterator/UnsafeIterator/UnsafeIterator+Value.swift
@@ -49,8 +49,10 @@ extension UnsafeIterator {
     @inlinable
     @inline(__always)
     public mutating func next() -> Base._PayloadValue? {
-      guard let p = _source.next() else { return nil }
-      return p.__value_().pointee
+      guard let p = _source.next() else {
+        return nil
+      }
+      return p.__value_().pointee as Base._PayloadValue
     }
   }
 }

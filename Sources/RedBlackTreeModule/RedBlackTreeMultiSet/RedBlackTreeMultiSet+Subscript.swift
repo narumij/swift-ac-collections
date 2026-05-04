fa06b5ce76a7144acddccb82c9e3ef705da9b82f
narumij <narumij@gmail.com>
2026-05-05 05:34:06 +0900

    subscript

--- Sources/RedBlackTreeModule/RedBlackTreeMultiSet/RedBlackTreeMultiSet+Subscript.swift
+++ Sources/RedBlackTreeModule/RedBlackTreeMultiSet/RedBlackTreeMultiSet+Subscript.swift
@@ -5,6 +5,26 @@
 //  Created by narumij on 2026/05/05.
 //
 
+#if !COMPATIBLE_ATCODER_2025 && false
+  // 追加するか検討
+  extension RedBlackTreeMultiSet {
+
+    /// - Complexity: O(log *n*)
+    @inlinable
+    @inline(__always)
+    public subscript(element: Element) -> View {
+      @inline(__always) get {
+        let (lower, upper) = ___equal_range(element)
+        return self[unchecked: .init(lowerBound: lower.sealed, upperBound: upper.sealed)]
+      }
+      @inline(__always) _modify {
+        let (lower, upper) = ___equal_range(element)
+        yield &self[unchecked: .init(lowerBound: lower.sealed, upperBound: upper.sealed)]
+      }
+    }
+  }
+#endif
+
 #if !COMPATIBLE_ATCODER_2025
   extension RedBlackTreeMultiSet {
 

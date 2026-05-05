bb8aa0ece09b108cd1c8ac8ea471c044c7653402
narumij <narumij@gmail.com>
2026-05-05 20:57:39 +0900

    comment

--- Sources/RedBlackTreeModule/RedBlackTreeDictionary/RedBlackTreeDictionary+Codable.swift
+++ Sources/RedBlackTreeModule/RedBlackTreeDictionary/RedBlackTreeDictionary+Codable.swift
@@ -10,6 +10,13 @@
 #if !COMPATIBLE_ATCODER_2025
   extension RedBlackTreeDictionary: Encodable where Key: Encodable, Value: Encodable {
 
+    /// Encodes the elements of this dictionary into the given encoder in an unkeyed
+    /// container.
+    ///
+    /// This function throws an error if any values are invalid for the given
+    /// encoder's format.
+    ///
+    /// - Parameter encoder: The encoder to write data to.
     @inlinable
     public func encode(to encoder: Encoder) throws {
       var container = encoder.unkeyedContainer()
@@ -21,6 +28,12 @@
 
   extension RedBlackTreeDictionary: Decodable where Key: Decodable, Value: Decodable {
 
+    /// Creates a new dictionary by decoding from the given decoder.
+    ///
+    /// This initializer throws an error if reading from the decoder fails, or
+    /// if the data read is corrupted or otherwise invalid.
+    ///
+    /// - Parameter decoder: The decoder to read data from.
     @inlinable
     public init(from decoder: Decoder) throws {
       self.init(__tree_: try .create(from: decoder))
--- Sources/RedBlackTreeModule/RedBlackTreeMultiSet/RedBlackTreeMultiSet+Codable.swift
+++ Sources/RedBlackTreeModule/RedBlackTreeMultiSet/RedBlackTreeMultiSet+Codable.swift
@@ -10,6 +10,13 @@
 #if !COMPATIBLE_ATCODER_2025
   extension RedBlackTreeMultiSet: Encodable where Element: Encodable {
 
+    /// Encodes the elements of this multi set into the given encoder in an unkeyed
+    /// container.
+    ///
+    /// This function throws an error if any values are invalid for the given
+    /// encoder's format.
+    ///
+    /// - Parameter encoder: The encoder to write data to.
     @inlinable
     public func encode(to encoder: Encoder) throws {
       var container = encoder.unkeyedContainer()
@@ -21,6 +28,12 @@
 
   extension RedBlackTreeMultiSet: Decodable where Element: Decodable {
 
+    /// Creates a new multi set by decoding from the given decoder.
+    ///
+    /// This initializer throws an error if reading from the decoder fails, or
+    /// if the data read is corrupted or otherwise invalid.
+    ///
+    /// - Parameter decoder: The decoder to read data from.
     @inlinable
     public init(from decoder: Decoder) throws {
       self.init(__tree_: try .create(from: decoder))

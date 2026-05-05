ef47404c8c3bab8c98e283445db9713fcb884cfa
narumij <narumij@gmail.com>
2026-05-06 02:19:37 +0900

    header

--- Sources/RedBlackTreeModule/RedBlackTreeMultiSet/RedBlackTreeMultiSet+ReserveCapacity.swift
+++ Sources/RedBlackTreeModule/RedBlackTreeMultiSet/RedBlackTreeMultiSet+ReserveCapacity.swift
@@ -1,9 +1,19 @@
+//===----------------------------------------------------------------------===//
 //
-//  RedBlackTreeMultiSet+ReserveCapacity.swift
-//  swift-ac-collections
+// This source file is part of the swift-ac-collections project
 //
-//  Created by narumij on 2026/05/05.
+// Copyright (c) 2024 - 2026 narumij.
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// This code is based on work originally distributed under the Apache License 2.0 with LLVM Exceptions:
+//
+// Copyright © 2003-2026 The LLVM Project.
+// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+// The original license can be found at https://llvm.org/LICENSE.txt
+//
+// This Swift implementation includes modifications and adaptations made by narumij.
+//
+//===----------------------------------------------------------------------===//
 
 extension RedBlackTreeMultiSet {
 
@@ -17,7 +27,7 @@ extension RedBlackTreeMultiSet {
 // MARK: -
 
 extension RedBlackTreeMultiSet {
-  
+
   /// Reserves enough space to store the specified number of elements.
   @inlinable
   public mutating func reserveCapacity(_ minimumCapacity: Int) {

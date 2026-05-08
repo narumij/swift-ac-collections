155d67f21c84e487f213e329ed1e70534f016d8a
narumij <narumij@gmail.com>
2026-05-09 05:08:38 +0900

    storage

--- Sources/RedBlackTreeModule/Implements/Memoize/___LRUMemoizeStorage.swift
+++ Sources/RedBlackTreeModule/Implements/Memoize/___LRUMemoizeStorage.swift
@@ -70,14 +70,14 @@ extension ___LRUMemoizeStorage {
 
     @inline(__always) mutating get {
       
-      let __ptr = __tree_.find(key)
+      let __ptr = __tree_.update { $0.find(key) }
       
       guard !__ptr.___is_null_or_end else {
         return nil
       }
       
       ___prepend(___pop(__ptr))
-      
+            
       return __tree_[_unsafe_raw: __ptr].value
     }
 
@@ -87,25 +87,30 @@ extension ___LRUMemoizeStorage {
         fatalError()
       }
 
-      if __tree_.count == maxCount {
-        _ = __tree_.erase(___popRankLowest())
-      }
-
       if __tree_.capacity < maxCount {
         // 無条件で更新するとサイズが安定せず、増加してしまう恐れがある
         __tree_.ensureCapacity(limit: maxCount)
       }
+      
+      let __h = __tree_.update { __tree_ in
+        
+        if __tree_.count == maxCount {
+          _ = __tree_.erase(___popRankLowest())
+        }
 
-      assert(__tree_.count < __tree_.capacity)
-
-      let (__parent, __child) = __tree_.__find_equal(key)
-
-      guard __child.__ptr_ == .nullptr else {
-        fatalError()
+        assert(__tree_.count < __tree_.capacity)
+        
+        let (__parent, __child) = __tree_.__find_equal(key)
+        
+        guard __child.__ptr_ == __tree_.nullptr else {
+          fatalError()
+        }
+        
+        let __h = __tree_.__construct_node(.init(key, __tree_.nullptr, __tree_.nullptr, newValue))
+        __tree_.__insert_node_at(__parent, __child, __h)
+        
+        return __h
       }
-
-      let __h = __tree_.__construct_node(.init(key, .nullptr, .nullptr, newValue))
-      __tree_.__insert_node_at(__parent, __child, __h)
       
       ___prepend(__h)
     }

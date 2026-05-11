bbc32a53571dca51451f88b74b0e55dd04203c6f
narumij <narumij@gmail.com>
2026-05-11 12:11:18 +0900

    pointer

--- Sources/RedBlackTreeModule/Implements/__tree/unsafe_node/unsafe_node+pointer.swift
+++ Sources/RedBlackTreeModule/Implements/__tree/unsafe_node/unsafe_node+pointer.swift
@@ -29,39 +29,37 @@ extension UnsafeMutablePointer where Pointee == UnsafeNode {
   @inlinable
   var __left_: _NodePtr {
     _read { yield pointee.__left_ }
-    _modify { yield &pointee.__left_ }
+    nonmutating _modify { yield &pointee.__left_ }
   }
 
   @inlinable
   var __right_: _NodePtr {
     _read { yield pointee.__right_ }
-    _modify { yield &pointee.__right_ }
+    nonmutating _modify { yield &pointee.__right_ }
   }
 
   @inlinable
   var __parent_: _NodePtr {
     _read { yield pointee.__parent_ }
-    _modify { yield &pointee.__parent_ }
+    nonmutating _modify { yield &pointee.__parent_ }
   }
   
   // NOTE: 移植の命名互換のための別名。意味は`__parent_`と同じ。
   @inlinable
   var __parent_unsafe: _NodePtr {
     _read { yield pointee.__parent_ }
-    _modify { yield &pointee.__parent_ }
+    nonmutating _modify { yield &pointee.__parent_ }
   }
 
-  // NOTE: 移植の命名互換のための別名。意味は`__parent_`と同じ。
   @inlinable
-  var __set_parent: _NodePtr {
-    _read { yield pointee.__parent_ }
-    _modify { yield &pointee.__parent_ }
+  func __set_parent(_ p: _NodePtr) {
+    pointee.__parent_ = p
   }
 
   @inlinable
   var __is_black_: Bool {
     _read { yield pointee.__is_black_ }
-    _modify { yield &pointee.__is_black_ }
+    nonmutating _modify { yield &pointee.__is_black_ }
   }
 
   @inlinable

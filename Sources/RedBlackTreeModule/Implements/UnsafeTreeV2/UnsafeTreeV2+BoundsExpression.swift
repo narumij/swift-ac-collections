696bceeb0e94374eaf9a899b8e4cdb864b3ff29c
narumij <narumij@gmail.com>
2026-05-12 20:12:25 +0900

    some recover

--- Sources/RedBlackTreeModule/Implements/UnsafeTreeV2/UnsafeTreeV2+BoundsExpression.swift
+++ Sources/RedBlackTreeModule/Implements/UnsafeTreeV2/UnsafeTreeV2+BoundsExpression.swift
@@ -19,7 +19,7 @@ import Foundation
 
 extension RedBlackTreeBoundExpression {
 
-  @inlinable
+  @inlinable @inline(__always)
   func evaluate<Base>(_ __tree_: UnsafeTreeV2<Base>)
     -> _SealedPtr
   where
@@ -106,7 +106,7 @@ extension RedBlackTreeBoundExpression {
 
 extension RedBlackTreeBoundRangeExpression {
 
-  @inlinable
+  @inlinable @inline(__always)
   func evaluate<Base>(_ __tree_: UnsafeTreeV2<Base>)
     -> _RawRangeExpression<_SealedPtr>
   where

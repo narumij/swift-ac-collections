1cff23a1e79765ad6bc283bc31c4845cefcb2436
narumij <narumij@gmail.com>
2026-05-14 00:13:16 +0900

    refactoring

--- Sources/RedBlackTreeModule/Implements/Deprecated/Protocol/_CompareV2+deprecated.swift
+++ Sources/RedBlackTreeModule/Implements/Deprecated/Protocol/_CompareV2+deprecated.swift
@@ -6,17 +6,8 @@
 //
 
 #if COMPATIBLE_ATCODER_2025
-@usableFromInline
-protocol _CompareV2: UnsafeTreeHostV2 & _KeyBride {}
-
-extension _CompareV2 where Base: CompareMultiTrait {
-
-  /// （重複あり）
-  @inlinable @inline(__always)
-  internal func ___equal_range(_ k: _Key) -> (lower: _NodePtr, upper: _NodePtr) {
-    __tree_.__equal_range_multi(k)
-  }
-}
+  @usableFromInline
+  protocol _CompareV2: UnsafeTreeHostV2 & _KeyBride {}
 #endif
 
 #if COMPATIBLE_ATCODER_2025
@@ -29,9 +20,16 @@ extension _CompareV2 where Base: CompareMultiTrait {
       __tree_.__equal_range_unique(k)
     }
   }
-#endif
 
-#if COMPATIBLE_ATCODER_2025
+  extension _CompareV2 where Base: CompareMultiTrait {
+
+    /// （重複あり）
+    @inlinable @inline(__always)
+    internal func ___equal_range(_ k: _Key) -> (lower: _NodePtr, upper: _NodePtr) {
+      __tree_.__equal_range_multi(k)
+    }
+  }
+
   // TODO: デッドコードになってないかチェックすること
   extension _CompareV2 where Base: CompareUniqueTrait, Self: UnsafeIndexProviderProtocolV2 {
 
@@ -44,6 +42,50 @@ extension _CompareV2 where Base: CompareMultiTrait {
   }
 
   extension _CompareV2 where Base: CompareMultiTrait, Self: UnsafeIndexProviderProtocolV2 {
+
+    /// （重複あり）
+    @inlinable @inline(__always)
+    internal func ___index_equal_range(_ k: _Key) -> (lower: Index, upper: Index) {
+      let (lo, hi) = __tree_.__equal_range_multi(k)
+      return (___index(lo.sealed), ___index(hi.sealed))
+    }
+  }
+#endif
+
+// MARK: -
+
+#if COMPATIBLE_ATCODER_2025
+  // TODO: デッドコードになってないかチェックすること
+  extension _CompareV2 where Base: CompareUniqueTraitHelper {
+
+    ///（重複なし）
+    @inlinable @inline(__always)
+    internal func ___equal_range(_ k: _Key) -> (lower: _NodePtr, upper: _NodePtr) {
+      __tree_.__equal_range_unique(k)
+    }
+  }
+
+  extension _CompareV2 where Base: CompareMultiTraitHelper {
+
+    /// （重複あり）
+    @inlinable @inline(__always)
+    internal func ___equal_range(_ k: _Key) -> (lower: _NodePtr, upper: _NodePtr) {
+      __tree_.__equal_range_multi(k)
+    }
+  }
+
+  // TODO: デッドコードになってないかチェックすること
+  extension _CompareV2 where Base: CompareUniqueTraitHelper, Self: UnsafeIndexProviderProtocolV2 {
+
+    ///（重複なし）
+    @inlinable @inline(__always)
+    internal func ___index_equal_range(_ k: _Key) -> (lower: Index, upper: Index) {
+      let (lo, hi) = ___equal_range(k)
+      return (___index(lo.sealed), ___index(hi.sealed))
+    }
+  }
+
+  extension _CompareV2 where Base: CompareMultiTraitHelper, Self: UnsafeIndexProviderProtocolV2 {
 
     /// （重複あり）
     @inlinable @inline(__always)

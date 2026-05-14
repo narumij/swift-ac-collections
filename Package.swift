dfab0afe37117a335c54c24863305d2786831cb0
narumij <narumij@gmail.com>
2026-05-14 09:43:31 +0900

    compatible

--- Package.swift
+++ Package.swift
@@ -18,15 +18,12 @@ var defines: [String] = [
   //  "ALLOCATION_DRILL" // リリース時はオフ
   //  "USE_C_MALLOC",
   //  "USE_INT128", // これはpackage traitにしたい
-//  "COLLECTION_BENCHMARK",
+  //  "COLLECTION_BENCHMARK",
 ]
 
 var _settings: [SwiftSetting] =
   [
-
-    
-    
-    .define("COMPATIBLE_ATCODER_2025"),
+    //    .define("COMPATIBLE_ATCODER_2025"),
     // このコードベースは当初、2025新ジャッジ搭載を目指して開発し、無事に搭載できました。
     // できましたが、引き続き開発をつづけており、APIの修正も含めて様々な改善をしています。
     // 過去版が単純なコード補完に反応しにくい設計だったこともあり、サポートプロジェクトでこちらを採用しています。

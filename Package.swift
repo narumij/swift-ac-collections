0421e8e98ef68d620c13d4b15d37fc882340aaad
narumij <narumij@gmail.com>
2026-05-16 07:33:54 +0900

    remove many targets

--- Package.swift
+++ Package.swift
@@ -18,7 +18,6 @@ var defines: [String] = [
   //  "ALLOCATION_DRILL" // リリース時はオフ
   //  "USE_C_MALLOC",
   //  "USE_INT128", // これはpackage traitにしたい
-    "COLLECTION_BENCHMARK",
 ]
 
 var _settings: [SwiftSetting] =
@@ -52,110 +51,9 @@ let additionalDepencencies: [Target.Dependency] =
   defines.contains("USE_C_MALLOC") ? ["_malloc_free"] : []
 
 let platforms: [SupportedPlatform]? =
-  defines.contains("USE_INT128") || defines.contains("COLLECTION_BENCHMARK")
+  defines.contains("USE_INT128")
   ? [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .watchOS(.v11), .macCatalyst(.v18)]
   : nil
-
-let collectionBenchmarks: [Target] =
-  defines.contains("COLLECTION_BENCHMARK")
-  ? (0...14).map { i in
-    .executableTarget(
-      name: "CollectionBenchmark\(i)",
-      dependencies: [
-        "RedBlackTreeModule",
-        .product(name: "CollectionsBenchmark", package: "swift-collections-benchmark"),
-        .product(name: "Collections", package: "swift-collections"),
-        .product(name: "SortedCollections", package: "swift-collections"),
-      ],
-      path: "Tests/CollectionBenchmarks/CollectionBenchmark\(i)",
-      swiftSettings: _settings
-    )
-  } : []
-
-let collectionDependency: [Package.Dependency] =
-  defines.contains("COLLECTION_BENCHMARK")
-  ? [
-    .package(
-      url: "https://github.com/apple/swift-collections-benchmark",
-      from: "0.0.0")
-  ]
-  : []
-
-// 順次削っていきたい
-let _mt19937: Target = .target(
-  name: "_MT19937",
-  path: "Utilities/_MT19937",
-  publicHeadersPath: "include",
-  cxxSettings: [
-    .headerSearchPath("include"),
-    .define("NDEBUG", .when(configuration: .release)),
-    .unsafeFlags(["-std=c++17"]),
-  ])
-
-// 順次削っていきたい
-let mt19937: Target = .target(
-  name: "MT19937",
-  dependencies: ["_MT19937"],
-  path: "Utilities/MT19937")
-
-// 順次削っていきたい
-let _fastIO: Target = .target(
-  name: "_FastIO",
-  path: "Utilities/_FastIO",
-  publicHeadersPath: "include",
-  cSettings: [
-    .headerSearchPath("include"),
-    .define("NDEBUG", .when(configuration: .release)),
-  ])
-
-// 順次削っていきたい
-let IOUtil: Target = .target(
-  name: "IOUtil",
-  dependencies: ["_FastIO"],
-  path: "Utilities/IOUtil",
-  swiftSettings: _settings)
-
-// 順次削っていきたい
-let executableTargets: [Target] =
-  [
-    "Executable",
-    "SimpleInsert",
-    "SimpleRemove",
-    "SimpleCreate",
-    "SimpleValue",
-    "ABC411F",
-    "LRU",
-  ]
-  .map { name in
-    .executableTarget(
-      name: "\(name)",
-      dependencies: [
-        "AcCollections",
-        "MT19937",
-        "IOUtil",
-        .product(name: "Collections", package: "swift-collections"),
-        .product(
-          name: "SortedCollections",
-          package: "swift-collections"),
-      ],
-      path: "Tests/Executables/\(name)")
-  }
-  + (0...4).map { i in
-    .executableTarget(
-      name: "Benchmark\(i)",
-      dependencies: [
-        "RedBlackTreeModule",
-        "MT19937",
-        "IOUtil",
-        .product(name: "Algorithms", package: "swift-algorithms"),
-        .product(name: "Benchmark", package: "swift-benchmark"),
-        .product(name: "Collections", package: "swift-collections"),
-      ],
-      path: "Tests/Benchmarks/Benchmark\(i)",
-      swiftSettings: _settings
-    )
-  }
-  + collectionBenchmarks
 
 let package = Package(
   name: "swift-ac-collections",
@@ -164,28 +62,9 @@ let package = Package(
   dependencies: [
 
     .package(
-      url: "https://github.com/apple/swift-collections.git",
-      branch: "main",
-      traits: ["UnstableSortedCollections"]
-    ),
-
-    .package(
       url: "https://github.com/apple/swift-algorithms.git",
-      from: "1.2.1"),
-
-    .package(
-      url: "https://github.com/google/swift-benchmark",
-      from: "0.1.0"),
-
-    //    .package(
-    //      url: "https://github.com/narumij/swift-ac-foundation",
-    //      branch: "main"),
-
-    //    .package(
-    //      url: "https://github.com/apple/swift-collections",
-    //      from: "1.3.0"),
-  ]
-  + collectionDependency,
+      from: "1.2.1")
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
@@ -210,7 +89,7 @@ let package = Package(
       path: "Sources/RedBlackTreeCollections",
       exclude: ["MEMO.md"],
       swiftSettings: _settings + [
-        //        .strictMemorySafety()
+        // .strictMemorySafety()
       ]),
 
     .testTarget(
@@ -235,11 +114,5 @@ let package = Package(
       ],
       swiftSettings: _settings
     ),
-
-    _mt19937,
-    mt19937,
-    _fastIO,
-    IOUtil,
   ]
-    + executableTargets
 )

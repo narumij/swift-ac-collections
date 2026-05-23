f38143c604fcd2effc74352212c2cae7a8a0aaa8
narumij <narumij@gmail.com>
2026-05-23 14:24:53 +0900

    benchmark

--- Sources/RedBlackTreeCollections/RedBlackTreeSet/RedBlackTreeSet+Benchmark.swift
+++ Sources/RedBlackTreeCollections/RedBlackTreeSet/RedBlackTreeSet+Benchmark.swift
@@ -9,23 +9,8 @@
   extension RedBlackTreeSet {
 
     @inlinable
-    public func __value_find(_ member: Element) -> Element? {
-      let p = __tree_.update { $0.find(member) }.safe
-//      return p != _end ? p.__value_(as: Element.self).pointee : nil
-      guard let p = p.pointer, !p.___is_end else { return nil }
-      return p.__value_(as: Element.self).pointee
-    }
-    
-    @inlinable
-    public func __raw_safe_find(_ member: Element) -> _SafePtr {
-      __tree_.update { $0.find(member) }.safe
-    }
-    
-    /// - Complexity: O( log `count` )
-    @inlinable
     public func __raw_find(_ member: Element) -> _NodePtr {
-//      __tree_.find(member)
-      __tree_.update { $0.find(member) }
+      __tree_.find(member)
     }
 
     @inlinable

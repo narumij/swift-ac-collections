05e51c885f79f790501ec8370bc151a19d74832d
narumij <narumij@gmail.com>
2026-05-24 10:59:12 +0900

    benchmark

---
+++ Benchmarks/diff2.sh
@@ -0,0 +1,32 @@
+swift run -c release benchmark results compare \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-2.0-20260524-100540-34a11e0b-adhoc.json \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.125-20260524-100921-34a11e0b-adhoc.json
+
+swift run -c release benchmark results compare \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-2.0-20260524-100540-34a11e0b-adhoc.json \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.25-20260524-101323-34a11e0b-adhoc.json
+
+swift run -c release benchmark results compare \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-2.0-20260524-100540-34a11e0b-adhoc.json \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.63-20260524-100145-34a11e0b-adhoc.json
+
+swift run -c release benchmark results compare \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-2.0-20260524-100540-34a11e0b-adhoc.json \
+  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.5-20260524-095756-34a11e0b-adhoc.json
+
+
+#swift run -c release benchmark results compare \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.5-20260524-095756-34a11e0b-adhoc.json \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.125-20260524-100921-34a11e0b-adhoc.json
+#
+#swift run -c release benchmark results compare \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.5-20260524-095756-34a11e0b-adhoc.json \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.25-20260524-101323-34a11e0b-adhoc.json
+#
+#swift run -c release benchmark results compare \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.5-20260524-095756-34a11e0b-adhoc.json \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.63-20260524-100145-34a11e0b-adhoc.json
+#
+#swift run -c release benchmark results compare \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-1.5-20260524-095756-34a11e0b-adhoc.json \
+#  /Users/narumij/Documents/GitHub/swift-ac-collections/Benchmarks/Results/2M/Adhoc/results-2.0-20260524-100540-34a11e0b-adhoc.json

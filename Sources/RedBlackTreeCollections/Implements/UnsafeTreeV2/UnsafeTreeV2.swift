5b994d62c3625b66e7caa7da1fb6a079fd9239a8
narumij <narumij@gmail.com>
2026-05-28 07:13:05 +0900

    lazy pointer

--- Sources/RedBlackTreeCollections/Implements/UnsafeTreeV2/UnsafeTreeV2.swift
+++ Sources/RedBlackTreeCollections/Implements/UnsafeTreeV2/UnsafeTreeV2.swift
@@ -251,7 +251,7 @@ extension UnsafeTreeV2 {
   /// 木が異なる場合、インデックスが保持するノード番号に対応するポインタを返す。
   @inlinable
   package func __purified_(_ index: _LazyTieWrappedPtr) -> _SealedPtr {
-    withMutableHeader { index.__isSameEnd($0.end_ptr) }
+    withMutableHeader { index.__isSameLazyDetach($0._lazyDetach) }
       // 木が同一のケース
       // 中身を取り出し、生存確認を行って返している
       ? index.sealed.purified

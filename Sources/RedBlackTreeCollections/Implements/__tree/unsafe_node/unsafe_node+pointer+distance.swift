4d682f3a40823e6573db8d30cb04523a35825307
narumij <narumij@gmail.com>
2026-05-28 02:06:48 +0900

    some

--- Sources/RedBlackTreeCollections/Implements/__tree/unsafe_node/unsafe_node+pointer+distance.swift
+++ Sources/RedBlackTreeCollections/Implements/__tree/unsafe_node/unsafe_node+pointer+distance.swift
@@ -15,25 +15,27 @@
 //
 //===----------------------------------------------------------------------===//
 
-// 遅い
-@inlinable
-internal func
-  ___dual_distance(
-    _ __first: UnsafeMutablePointer<UnsafeNode>,
-    _ __last: UnsafeMutablePointer<UnsafeNode>
-  )
-  -> Int
-{
-  var __next = __first
-  var __prev = __first
-  var __r = 0
-  while __next != __last, __prev != __last {
-    __next = __next.___is_null ? __next : __tree_next(__next)
-    __prev = __prev.___is_null ? __prev : __tree_prev_iter(__prev)
-    __r += 1
+#if false
+  // 遅い
+  @inlinable
+  internal func
+    ___dual_distance(
+      _ __first: UnsafeMutablePointer<UnsafeNode>,
+      _ __last: UnsafeMutablePointer<UnsafeNode>
+    )
+    -> Int
+  {
+    var __next = __first
+    var __prev = __first
+    var __r = 0
+    while __next != __last, __prev != __last {
+      __next = __next.___is_null ? __next : __tree_next(__next)
+      __prev = __prev.___is_null ? __prev : __tree_prev_iter(__prev)
+      __r += 1
+    }
+    return __next == __last ? __r : -__r
   }
-  return __next == __last ? __r : -__r
-}
+#endif
 
 @inlinable
 internal func

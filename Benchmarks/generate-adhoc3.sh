bce62fe7dfb2538142361d56f2d32fc6823bf9dc
narumij <narumij@gmail.com>
2026-05-28 07:14:00 +0900

    bench

--- Benchmarks/generate-adhoc3.sh
+++ Benchmarks/generate-adhoc3.sh
@@ -2,18 +2,18 @@ STAMP=$(date +%Y%m%d-%H%M%S)
 COMMIT=$(git rev-parse --short HEAD)
 ID=${STAMP}-${COMMIT}-adhoc
 
-mkdir -p ./Results/2M/Adhoc2
+mkdir -p ./Results/2M/Adhoc3
 
 swift run -c release benchmark library run \
-  --library ./Libraries/Adhoc2.json \
-  ./Results/2M/Adhoc2/results-${ID}.json \
+  --library ./Libraries/Adhoc3.json \
+  ./Results/2M/Adhoc3/results-${ID}.json \
   --max-size 2M \
   --cycles 3 \
   --mode replace-all
 
 swift run -c release benchmark library render \
-  --library ./Libraries/Adhoc2.json \
-  ./Results/2M/Adhoc2/results-${ID}.json \
+  --library ./Libraries/Adhoc3.json \
+  ./Results/2M/Adhoc3/results-${ID}.json \
   --max-time 10us \
   --min-time 1ns \
   --percentile 90 \

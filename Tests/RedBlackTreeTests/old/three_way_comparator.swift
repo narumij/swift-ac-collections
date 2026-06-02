fa357b1333d1dc098fd4de1bd1f3bb014a5426d7
narumij <narumij@gmail.com>
2026-06-03 06:38:33 +0900

    fix compile error

--- Tests/RedBlackTreeTests/old/three_way_comparator.swift
+++ Tests/RedBlackTreeTests/old/three_way_comparator.swift
@@ -4,16 +4,19 @@
 //
 //  Created by narumij on 2026/06/01.
 //
+#if DEBUG
+  @testable import RedBlackTreeCollections
 
-@inlinable
-package func ___default_three_way_comparator<T: Comparable>(_ __lhs: T, _ __rhs: T)
-  -> ___enum_compare_result
-{
-  if __lhs < __rhs {
-    .less
-  } else if __lhs > __rhs {
-    .greater
-  } else {
-    .equal
+  @inlinable
+  package func ___default_three_way_comparator<T: Comparable>(_ __lhs: T, _ __rhs: T)
+    -> ___enum_compare_result
+  {
+    if __lhs < __rhs {
+      .less
+    } else if __lhs > __rhs {
+      .greater
+    } else {
+      .equal
+    }
   }
-}
+#endif

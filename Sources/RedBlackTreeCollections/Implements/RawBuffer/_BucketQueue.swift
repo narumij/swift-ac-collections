953acf0efd45bf8d10916739d7bc28c596117945
narumij <narumij@gmail.com>
2026-09-21 23:53:42 +0900

    pair layout

--- Sources/RedBlackTreeCollections/Implements/RawBuffer/_BucketQueue.swift
+++ Sources/RedBlackTreeCollections/Implements/RawBuffer/_BucketQueue.swift
@@ -83,4 +83,7 @@ extension MemoryLayout where T: ~Copyable {
 
   @inlinable
   static var _memoryLayout: _MemoryLayout { .init(stride: stride, alignment: alignment) }
+  
+  @inlinable
+  static var _pairLayout: _MemoryLayout { .init(UnsafeNode.self, T.self) }
 }

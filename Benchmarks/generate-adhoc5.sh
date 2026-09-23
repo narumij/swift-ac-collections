a06c9a5719802efb1d347e3fccb3b0f089ce89b4
narumij <narumij@gmail.com>
2026-09-24 04:16:35 +0900

    adhoc5 bench

--- Benchmarks/generate-adhoc5.sh
+++ Benchmarks/generate-adhoc5.sh
@@ -2,18 +2,18 @@ STAMP=$(date +%Y%m%d-%H%M%S)
 COMMIT=$(git rev-parse --short HEAD)
 ID=${STAMP}-${COMMIT}-adhoc
 
-mkdir -p ./Results/2M/Adhoc5
+mkdir -p ./Results/512k/Adhoc5
 
 swift run -c release benchmark library run \
   --library ./Libraries/AdHoc5.json \
-  ./Results/2M/Adhoc5/results-${ID}.json \
-  --max-size 2M \
+  ./Results/512k/Adhoc5/results-${ID}.json \
+  --max-size 512k \
   --cycles 1 \
   --mode replace-all
 
 swift run -c release benchmark library render \
   --library ./Libraries/AdHoc5.json \
-  ./Results/2M/Adhoc5/results-${ID}.json \
+  ./Results/512k/Adhoc5/results-${ID}.json \
   --max-time 10us \
   --min-time 1ns \
   --percentile 90 \

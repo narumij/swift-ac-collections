b981ea8f6ac6eaae52098eb47c34e16f2390af55
narumij <narumij@gmail.com>
2026-09-26 04:18:15 +0900

    long

--- Benchmarks/generate-long.sh
+++ Benchmarks/generate-long.sh
@@ -1,19 +1,19 @@
 STAMP=$(date +%Y%m%d-%H%M%S)
 COMMIT=$(git rev-parse --short HEAD)
-ID=${STAMP}-${COMMIT}-adhoc
+ID=${STAMP}-${COMMIT}-long
 
-mkdir -p ./Results/16M/Adhoc6
+mkdir -p ./Results/16M/Long
 
 swift run -c release benchmark library run \
-  --library ./Libraries/AdHoc.json \
-  ./Results/16M/Adhoc6/results-${ID}.json \
+  --library ./Libraries/Long.json \
+  ./Results/16M/Long/results-${ID}.json \
   --max-size 16M \
   --cycles 1 \
   --mode replace-all
 
 swift run -c release benchmark library render \
-  --library ./Libraries/AdHoc.json \
-  ./Results/16M/Adhoc6/results-${ID}.json \
+  --library ./Libraries/Long.json \
+  ./Results/16M/Long/results-${ID}.json \
   --max-time 10us \
   --min-time 1ns \
   --percentile 90 \

a119e7b10a7f631c04ddf10dc6dab08642b753f7
narumij <narumij@gmail.com>
2026-09-26 14:03:13 +0900

    fix test

--- Tests/RedBlackTreeTests/DocumentCheckTests.swift
+++ Tests/RedBlackTreeTests/DocumentCheckTests.swift
@@ -8,15 +8,7 @@
 import RedBlackTreeCollections
 import XCTest
 
-final class DocumentCheckTests: XCTestCase {
-
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
+final class DocumentCheckTests: RedBlackTreeTestCase {
 
   func testExample1() throws {
     var numbers = RedBlackTreeSet<Int>(0..<10)
